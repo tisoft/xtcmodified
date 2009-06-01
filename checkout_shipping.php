eb18f8f29c4f26c5d56e48e058d8f40b5c4ea4f2
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2009-06-01 14:55:57 +0000

    do not correct the name 'BUTON_CONTINUE' to remain compatible to standard templates
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@47 25741c12-d480-4641-b65d-acd752be3b7f

--- checkout_shipping.php
+++ checkout_shipping.php
@@ -181,6 +181,7 @@ require (DIR_WS_INCLUDES.'header.php');
 $smarty->assign('FORM_ACTION', xtc_draw_form('checkout_address', xtc_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL')).xtc_draw_hidden_field('action', 'process'));
 $smarty->assign('ADDRESS_LABEL', xtc_address_label($_SESSION['customer_id'], $_SESSION['sendto'], true, ' ', '<br />'));
 $smarty->assign('BUTTON_ADDRESS', '<a href="'.xtc_href_link(FILENAME_CHECKOUT_SHIPPING_ADDRESS, '', 'SSL').'">'.xtc_image_button('button_change_address.gif', IMAGE_BUTTON_CHANGE_ADDRESS).'</a>');
+// DokuMan - 2009-06-01 - do not correct the name 'BUTON_CONTINUE' to remain compatible to standard templates
 $smarty->assign('BUTON_CONTINUE', xtc_image_submit('button_continue.gif', IMAGE_BUTTON_CONTINUE));
 $smarty->assign('FORM_END', '</form>');
 
