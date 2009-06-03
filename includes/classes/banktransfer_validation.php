aeefa7a3622eba183af8cbfdbaff17dcd326f73e
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2009-06-03 14:24:03 +0000

    Wrong selection criteria on BLZ
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@57 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/classes/banktransfer_validation.php
+++ includes/classes/banktransfer_validation.php
@@ -63,7 +63,10 @@ var $PRZ; //Enthält die Prüfziffer
 ////
 // Diese function gibt die Bankinformationen aus der Datenbank zurück*/
   function db_query($blz) {
-    $blz_query = xtc_db_query("SELECT * from banktransfer WHERE blz = '" . $blz . "'");
+    //BOF - Dokuman - 2009-06-03 - Wrong selection criteria on BLZ
+    //$blz_query = xtc_db_query("SELECT * from banktransfer WHERE blz = '" . $blz . "'");   
+     $blz_query = xtc_db_query("SELECT * from banktransfer WHERE banktransfer_blz = '" . $blz . "'");
+    //BOF - Dokuman - 2009-06-03 - Wrong selection criteria on BLZ
     if (xtc_db_num_rows($blz_query)){
       $data = xtc_db_fetch_array ($blz_query);
     }else
