fdae6b6fbc20fba6c78d614ffc7a0eda45539085
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2009-08-08 23:55:59 +0000

    updated thickbox links due to doubled quotation marks, see also: http://www.xtc-modified.org/forum/topic.php?id=1535
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@192 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/classes/main.php
+++ includes/classes/main.php
@@ -45,10 +45,10 @@ class main {
  	}
  	
  	function getShippingLink() {
- 	  //BOF - DokuMan - 2009-05-31 - fixed wrong quotationmark position
+ 	  //BOF - DokuMan - 2009-08-09 - fixed wrong quotationmark position and fixed wrong question mark on KeepThis=true
  	  //return ' '.SHIPPING_EXCL.' <a target="blank" href="'.xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.SHIPPING_INFOS.'?KeepThis=true&TB_iframe=true&height=400&width=600"').' title="Information" class="thickbox">'.SHIPPING_COSTS.'</a>';
-	return ' '.SHIPPING_EXCL.' <a target="_blank" href="'.xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.SHIPPING_INFOS.'?KeepThis=true&TB_iframe=true&height=400&width=600').'" title="Information" class="thickbox">'.SHIPPING_COSTS.'</a>';
-	  //EOF - DokuMan - 2009-05-31 - fixed wrong quotationmark position
+	return ' '.SHIPPING_EXCL.' <a target="_blank" href="'.xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.SHIPPING_INFOS.'&KeepThis=true&TB_iframe=true&height=400&width=600').'" title="Information" class="thickbox">'.SHIPPING_COSTS.'</a>';
+	  //EOF - DokuMan - 2009-05-31 - fixed wrong quotationmark position and fixed wrong question mark on KeepThis=true
 	}
 
 	function getTaxNotice() {
@@ -97,10 +97,10 @@ class main {
 	}
 	
    function getContentLink($coID,$text,$ssl='') {
- 	  //BOF - Hetfield - 2009-07-29 - SSL for Content-Links per getContentLink
+ 	  //BOF - Hetfield - 2009-07-29 - SSL for Content-Links per getContentLink and fixed wrong question mark on KeepThis=true	  
 		if ($ssl != 'SSL' ) { $ssl = 'NONSSL'; }
-		return '<a target="_blank" href="'.xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.$coID.'?KeepThis=true&TB_iframe=true&height=400&width=600"', $ssl).' title="Information" class="thickbox"><font color="#ff0000">'.$text.'</font></a>';
-		//EOF - Hetfield - 2009-07-29 - SSL for Content-Links per getContentLink
+		return '<a target="_blank" href="'.xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.$coID.'&KeepThis=true&TB_iframe=true&height=400&width=600"', $ssl).' title="Information" class="thickbox"><font color="#ff0000">'.$text.'</font></a>';
+		//EOF - Hetfield - 2009-07-29 - SSL for Content-Links per getContentLink and fixed wrong question mark on KeepThis=true	  
 	 }
 
 }
--- templates/xtc5/source/boxes/shopping_cart.php
+++ templates/xtc5/source/boxes/shopping_cart.php
@@ -69,10 +69,10 @@ if ($_SESSION['customers_status']['customers_status_show_price'] == '1') {
 	$box_smarty->assign('UST', $_SESSION['cart']->show_tax());
 	
 	if (SHOW_SHIPPING=='true') { 
-    //BOF - DokuMan - 2009-05-31 - added blank outside anchor, removed needless quotation mark
+ 	  //BOF - DokuMan - 2009-08-09 - fixed wrong quotationmark position and fixed wrong question mark on KeepThis=true
 		//$box_smarty->assign('SHIPPING_INFO',' '.SHIPPING_EXCL.'<a target="_blank" href="'.xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.SHIPPING_INFOS.'?KeepThis=true&TB_iframe=true&height=400&width=600"').' title="Information" class="thickbox"">'.SHIPPING_COSTS.'</a>');	
-		$box_smarty->assign('SHIPPING_INFO',' '.SHIPPING_EXCL.' <a target="_blank" href="'.xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.SHIPPING_INFOS.'?KeepThis=true&TB_iframe=true&height=400&width=600', 'SSL').'" title="Information" class="thickbox">'.SHIPPING_COSTS.'</a>');	
-    //EOF - DokuMan - 2009-05-31 - added blank outside anchor, removed needless quotation mark
+		$box_smarty->assign('SHIPPING_INFO',' '.SHIPPING_EXCL.' <a target="_blank" href="'.xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.SHIPPING_INFOS.'&KeepThis=true&TB_iframe=true&height=400&width=600', 'SSL').'" title="Information" class="thickbox">'.SHIPPING_COSTS.'</a>');	
+ 	  //EOF - DokuMan - 2009-08-09 - fixed wrong quotationmark position and fixed wrong question mark on KeepThis=true
 	}
 }
 if (ACTIVATE_GIFT_SYSTEM == 'true') {
