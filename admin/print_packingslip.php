eefd84dfdb3c5168a4b9b1623c4f15f121f776ea
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2009-08-08 21:52:56 +0000

    Update print_order and packing_slip in order to also display order comments!
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@189 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/print_packingslip.php
+++ admin/print_packingslip.php
@@ -25,7 +25,7 @@
   $order_query_check = xtc_db_query("SELECT
   					customers_id
   					FROM ".TABLE_ORDERS."
-  					WHERE orders_id='".$_GET['oID']."'");
+  					WHERE orders_id='".(int)$_GET['oID']."'");
   					
   $order_check = xtc_db_fetch_array($order_query_check);
  // if ($_SESSION['customer_id'] == $order_check['customers_id'])
@@ -34,6 +34,8 @@
  	
   	include(DIR_WS_CLASSES . 'order.php');
   	$order = new order($_GET['oID']);
+
+
   	$smarty->assign('address_label_customer',xtc_address_format($order->customer['format_id'], $order->customer, 1, '', '<br />'));
   	$smarty->assign('address_label_shipping',xtc_address_format($order->delivery['format_id'], $order->delivery, 1, '', '<br />'));
   	$smarty->assign('address_label_payment',xtc_address_format($order->billing['format_id'], $order->billing, 1, '', '<br />'));
@@ -47,7 +49,7 @@
         				final_price,
         				products_quantity
         				FROM ".TABLE_ORDERS_PRODUCTS."
-        				WHERE orders_id='".$_GET['oID']."'");
+        				WHERE orders_id='".(int)$_GET['oID']."'");
         $order_data=array();
         while ($order_data_values = xtc_db_fetch_array($order_query)) {
         	$attributes_query=xtc_db_query("SELECT
@@ -102,6 +104,7 @@
      $payment_method=constant(strtoupper('MODULE_PAYMENT_'.$order->info['payment_method'].'_TEXT_TITLE'));
       $smarty->assign('PAYMENT_METHOD',$payment_method);
     }
+      $smarty->assign('COMMENTS', $order->info['comments']);
       $smarty->assign('DATE',xtc_date_long($order->info['date_purchased']));
       $smarty->assign('order_data', $order_data);
       $smarty->assign('order_total', $order_total);
