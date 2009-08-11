b4f483fc44de9f6ceca2527b21c67f01b69008c1
jhetfield <jhetfield@25741c12-d480-4641-b65d-acd752be3b7f>
2009-08-11 18:20:29 +0000

    no longer empty site products_new.php
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@208 25741c12-d480-4641-b65d-acd752be3b7f

--- products_new.php
+++ products_new.php
@@ -75,10 +75,10 @@ $products_new_query_raw = "select distinct
                                     ".$days."
                            order by p.products_date_added DESC ";
 
-
 $products_new_split = new splitPageResults($products_new_query_raw, $_GET['page'], MAX_DISPLAY_PRODUCTS_NEW, 'p.products_id');
-
+//BOF - Hetfield - 2009-08-11 - no longer empty site products_new.php
 if (($products_new_split->number_of_rows > 0)) {
+$module_content = '';
 //BOF - Hetfield - 2009-08-11 - replace table with div
 	$smarty->assign('NAVIGATION_BAR', '
 		   <div style="width:100%;font-size:smaller">
@@ -88,10 +88,6 @@ if (($products_new_split->number_of_rows > 0)) {
 		   </div>		
 		   ');
 //EOF - Hetfield - 2009-08-11 - replace table with div
-}
-
-$module_content = '';
-if ($products_new_split->number_of_rows > 0) {
 	$products_new_query = xtc_db_query($products_new_split->sql_query);
 	while ($products_new = xtc_db_fetch_array($products_new_query)) {
 		//BOF - Hetfield - 2009-08-11 - products_new uses now the product class
@@ -99,11 +95,44 @@ if ($products_new_split->number_of_rows > 0) {
 		//EOF - Hetfield - 2009-08-11 - products_new uses now the product class
 	}
 } else {
-
-	$smarty->assign('ERROR', TEXT_NO_NEW_PRODUCTS);
-
+	$new_products_query = "select distinct
+                                    p.products_id,
+                                    p.products_fsk18,
+                                    pd.products_name,
+                                    pd.products_short_description,
+                                    p.products_image,
+                                    p.products_price,
+                               	    p.products_vpe,
+                               	    p.products_vpe_status,
+                               	    p.products_vpe_value,
+                                    p.products_tax_class_id,
+                                    p.products_date_added,
+									m.manufacturers_name
+                           from ".TABLE_PRODUCTS." p
+                           left join ".TABLE_MANUFACTURERS." m
+                                    on p.manufacturers_id = m.manufacturers_id
+                           left join ".TABLE_PRODUCTS_DESCRIPTION." pd
+                                    on p.products_id = pd.products_id,
+                                    ".TABLE_CATEGORIES." c,
+                                    ".TABLE_PRODUCTS_TO_CATEGORIES." p2c 
+                           where pd.language_id = '".(int) $_SESSION['languages_id']."'
+                           and c.categories_status=1
+                           and p.products_id = p2c.products_id
+                           and c.categories_id = p2c.categories_id
+                           and products_status = '1'
+                                    ".$group_check."
+                                    ".$fsk_lock."
+	                       order by p.products_date_added DESC limit 10";
+						   
+	$module_content = array ();
+	$new_products_query = xtDBquery($new_products_query);
+	while ($new_products = xtc_db_fetch_array($new_products_query, true)) {
+		$module_content[] = $product->buildDataArray($new_products);
+	
+	}
+	$smarty->assign('NO_NEW_PRODUCTS', TEXT_NO_NEW_PRODUCTS);
 }
-
+//EOF - Hetfield - 2009-08-11 - no longer empty site products_new.php
 $smarty->assign('language', $_SESSION['language']);
 $smarty->caching = 0;
 $smarty->assign('module_content', $module_content);
