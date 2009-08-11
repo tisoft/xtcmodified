6c38286567c7baa9ec7b6b1d674144a84309ea81
jhetfield <jhetfield@25741c12-d480-4641-b65d-acd752be3b7f>
2009-08-11 00:03:44 +0000

    BUGFIX: #0000211 wrong modelnumbers on atrributes
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@198 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_get_attributes_model.inc.php
+++ inc/xtc_get_attributes_model.inc.php
@@ -15,8 +15,9 @@
    
 	function xtc_get_attributes_model($product_id, $attribute_name,$options_name,$language='')
     {
-    if ($language=='') $language=$_SESSION['languages_id'];
-    $options_value_id_query=xtc_db_query("SELECT
+	if ($language=='') $language=(int)$_SESSION['languages_id'];
+	//BOF - Hetfield - 2009-08-11 - BUGFIX: #0000211 wrong modelnumbers on atrributes
+	$options_value_id_query=xtc_db_query("SELECT
 		pa.attributes_model
 		FROM
 		".TABLE_PRODUCTS_ATTRIBUTES." pa
@@ -24,11 +25,12 @@
 		INNER JOIN ".TABLE_PRODUCTS_OPTIONS_VALUES." pov ON pa.options_values_id = pov.products_options_values_id
 		WHERE
 		po.language_id = '".$language."' AND
+		pa.products_id = '".$product_id."' AND
 		po.products_options_name = '".$options_name."' AND
 		pov.language_id = '".$language."' AND
 		pov.products_options_values_name = '".$attribute_name."' AND 
 		pa.products_id='".$product_id."'");
-
+    //EOF - Hetfield - 2009-08-11 - BUGFIX: #0000211 wrong modelnumbers on atrributes
     $options_attr_data = xtc_db_fetch_array($options_value_id_query);
     return $options_attr_data['attributes_model'];	
     	
