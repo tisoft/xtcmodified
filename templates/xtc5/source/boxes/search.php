1dafa0f826a628e84559834d000da89b4204fb68
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2009-08-14 11:53:12 +0000

    Put dynamic "search"-text into box using JavaScript
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@226 25741c12-d480-4641-b65d-acd752be3b7f

--- templates/xtc5/source/boxes/search.php
+++ templates/xtc5/source/boxes/search.php
@@ -24,7 +24,10 @@ require_once (DIR_FS_INC . 'xtc_image_submit.inc.php');
 require_once (DIR_FS_INC . 'xtc_hide_session_id.inc.php');
 
 $box_smarty->assign('FORM_ACTION', xtc_draw_form('quick_find', xtc_href_link(FILENAME_ADVANCED_SEARCH_RESULT, '', 'NONSSL', false), 'get') . xtc_hide_session_id());
-$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', '', 'size="20" maxlength="30"'));
+//BOF - Dokuman - 14.08.2009 - Put dynamic "search"-text into box
+//$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', '', 'size="20" maxlength="30"'));
+$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', IMAGE_BUTTON_SEARCH, 'size="20" maxlength="30" onfocus="if(this.value==this.defaultValue) this.value=\'\';" onblur="if(this.value==\'\') this.value=this.defaultValue;"'));
+//EOF - Dokuman - 14.08.2009 - Put dynamic "search"-text into box
 $box_smarty->assign('BUTTON_SUBMIT', xtc_image_submit('button_quick_find.gif', IMAGE_BUTTON_SEARCH));
 $box_smarty->assign('FORM_END', '</form>');
 $box_smarty->assign('LINK_ADVANCED', xtc_href_link(FILENAME_ADVANCED_SEARCH));
