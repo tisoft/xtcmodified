1f21722e6911c2cfaff72e88690b02c2741225a3
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2009-08-27 13:33:01 +0000

    Fixed 'preg_match' warnings in server_info.php
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@264 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/server_info.php
+++ admin/server_info.php
@@ -103,9 +103,9 @@
     ob_end_clean();
 
     $phpinfo = str_replace('border: 1px', '', $phpinfo);
-    preg_match("/(<style type=\"text/css\">{1})(.*)(</style>{1})/", $phpinfo, $regs); // Hetfield - 2009-08-19 - replaced depricated function ereg with preg_match to be ready for PHP >= 5.3
+    preg_match('/(<style type=\"text\/css\">{1})(.*)(<\/style>{1})/s', $phpinfo, $regs); // Hetfield - 2009-08-19 - replaced depricated function ereg with preg_match to be ready for PHP >= 5.3
     echo '<style type="text/css">' . $regs[2] . '</style>';
-    preg_match("/(<body>{1})(.*)(</body>{1})/", $phpinfo, $regs); // Hetfield - 2009-08-19 - replaced depricated function ereg with preg_match to be ready for PHP >= 5.3
+    preg_match('/(<body>{1})(.*)(<\/body>{1})/s', $phpinfo, $regs); // Hetfield - 2009-08-19 - replaced depricated function ereg with preg_match to be ready for PHP >= 5.3
     echo $regs[2];
   } else {
     phpinfo();
