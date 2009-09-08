883394a535b563bd7304a6c62557f7b2845d994b
Tomcraft1980 <Tomcraft1980@25741c12-d480-4641-b65d-acd752be3b7f>
2009-09-08 15:50:26 +0000

    updated and modified the update.sql statements for specific versions
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@289 25741c12-d480-4641-b65d-acd752be3b7f

---
+++ xtc_installer/update_1.01_to_1.02.sql
@@ -0,0 +1,10 @@
+#Add content metatags functionality
+ALTER TABLE `content_manager` ADD `content_meta_title` TEXT,
+ADD `content_meta_description` TEXT,
+ADD `content_meta_keywords` TEXT;
+
+ALTER TABLE `content_manager` ADD FULLTEXT (
+`content_meta_title` ,
+`content_meta_description` ,
+`content_meta_keywords`
+);
