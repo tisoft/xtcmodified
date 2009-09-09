9a507186faccfcf1168629fa1624a96d7fb0e562
Tomcraft1980 <Tomcraft1980@25741c12-d480-4641-b65d-acd752be3b7f>
2009-09-09 15:12:15 +0000

    small addition on r288:289
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@293 25741c12-d480-4641-b65d-acd752be3b7f

--- xtc_installer/update_1.01_to_1.02.sql
+++ xtc_installer/update_1.01_to_1.02.sql
@@ -8,3 +8,6 @@ ALTER TABLE `content_manager` ADD FULLTEXT (
 `content_meta_description` ,
 `content_meta_keywords`
 );
+
+#Tomcraft - 2009-09-09 - changed database_version
+UPDATE database_version SET version = 'xtcM_1.0.2.0'
