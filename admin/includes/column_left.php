ab724e9ff2e08b2dc7dec52c640d864fc4be1ce0
Tomcraft1980 <Tomcraft1980@25741c12-d480-4641-b65d-acd752be3b7f>
2009-11-02 14:46:12 +0000

    Show customers-group-permission only when group-check is enabled!
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@463 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/includes/column_left.php
+++ admin/includes/column_left.php
@@ -68,7 +68,9 @@
   if (($_SESSION['customers_status']['customers_status_id'] == '0') && ($admin_access['whos_online'] == '1')) echo '<a href="' . xtc_href_link(FILENAME_WHOS_ONLINE) . '" class="menuBoxContentLink"> -' . BOX_WHOS_ONLINE . '</a><br />';
   if (($_SESSION['customers_status']['customers_status_id'] == '0') && ($admin_access['csv_backend'] == '1')) echo '<a href="' . xtc_href_link('csv_backend.php') . '" class="menuBoxContentLink"> -' . BOX_IMPORT . '</a><br />';
 // BOF - Tomcraft - 2009-10-03 - Paypal Express Modul
+if (GROUP_CHECK=='true') {
   if (($_SESSION['customers_status']['customers_status_id'] == '0') && ($admin_access['paypal'] == '1')) echo '<a href="' . xtc_href_link('paypal.php') . '" class="menuBoxContentLink"> -' . BOX_PAYPAL . '</a><br>';
+  }
 // EOF - Tomcraft - 2009-10-03 - Paypal Express Modul
 
 
