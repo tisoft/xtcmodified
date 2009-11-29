4afbac770654806867c143b2e15ce59b4873eafe
Tomcraft1980 <Tomcraft1980@25741c12-d480-4641-b65d-acd752be3b7f>
2009-11-29 11:53:09 +0000

    Next try on r564, replaced ECB Patch with bugfixed Ruhrmedia Patch again.
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@570 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_db_prepare_input.inc.php
+++ inc/xtc_db_prepare_input.inc.php
@@ -1,50 +1,45 @@
 <?php
-/* --------------------------------------------------------------
-   xtc_db_prepare_input.inc.php 2009-11-27 gambio
-   Gambio GmbH
-   http://www.gambio.de
-   Copyright (c) 2009 Gambio GmbH
-   Released under the GNU General Public License
-   --------------------------------------------------------------
-*/
-?><?php
 /* -----------------------------------------------------------------------------------------
-   $Id: xtc_db_prepare_input.inc.php 899 2005-04-29 02:40:57Z hhgag $
+   $Id: xtc_db_prepare_input.inc.php 528 2009-11-28 17:14:40Z dis $   
 
    XT-Commerce - community made shopping
    http://www.xt-commerce.com
 
    Copyright (c) 2003 XT-Commerce
    -----------------------------------------------------------------------------------------
-   based on:
+   based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(database.php,v 1.19 2003/03/22); www.oscommerce.com
+   (c) 2002-2003 osCommerce(database.php,v 1.19 2003/03/22); www.oscommerce.com 
    (c) 2003	 nextcommerce (xtc_db_prepare_input.inc.php,v 1.4 2003/08/13); www.nextcommerce.org
 
-   Released under the GNU General Public License
-   ---------------------------------------------------------------------------------------*/
-
-function xtc_db_prepare_input($string)
-{
-	if (is_string($string))
-	{
-		$string = preg_replace('/union.*select.*from/i', '', $string);
-		return trim(stripslashes($string));
-	}
-	elseif (is_array($string))
-	{
-		reset($string);
-		while (list($key, $value) = each($string))
-		{
-			$string[$key] = xtc_db_prepare_input($value);
-		}
-		return $string;
-	}
-	else
-	{
-		return $string;
-	}
-}
-
-
-?>
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/  
+  function xtc_db_prepare_input($string) {
+  	$rv = '';
+    if (is_string($string)) {
+      $rv = trim(stripslashes($string));
+      return ai_clean_db_input($rv);
+    } elseif (is_array($string)) {
+      reset($string);
+      while (list($key, $value) = each($string)) {
+        $string[$key] = xtc_db_prepare_input($value);
+      }
+      return $string;
+    } else {
+      return ($string);
+    }
+  }
+  
+  function ai_clean_db_input($string) {
+  	$rv = $string;
+  	if(preg_match('/select.*from/i', $rv)) {
+  		$rv = preg_replace('/(.*?)\s*(?:union)?\s*select.*from.*/i', '$1', $rv);
+  		if($lc = substr($rv, strlen($rv) - 1, 1)) {
+  			if($lc  == "'" || $lc == '"') {
+  			 $rv = substr($rv, 0, strlen($rv) -1);
+  			}
+  		}
+  	}
+  	return $rv;
+  }
+ ?>
