800ddb247d445563dd6fac66fd7c3190d78b258a
tonne1 <tonne1@25741c12-d480-4641-b65d-acd752be3b7f>
2009-12-04 02:09:30 +0000

    credits internationalization
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@587 25741c12-d480-4641-b65d-acd752be3b7f

---
+++ lang/english/admin/credits.php
@@ -0,0 +1,29 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: credits.php ???? 2009-12-04 01:19:00Z vr $   
+
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
+
+   Copyright (c) 2009 xtcModified
+   --------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(customers.php,v 1.13 2002/06/15); www.oscommerce.com 
+   (c) 2003	 nextcommerce (customers.php,v 1.8 2003/08/15); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Credits');
+
+define('TEXT_HEADING_DEVELOPERS', 'Developers of xtcModified eCommerce shop software:');
+define('TEXT_HEADING_SUPPORT', 'Please support further development:');
+define('TEXT_HEADING_DONATIONS', 'Donations:');
+define('TEXT_HEADING_BASED_ON', 'This shop software is based on:');
+
+define('TEXT_INFO_THANKS', 'We wish to thank all coders and developers involved in this project. In case we forgot to mention anyone in the listing below, please post a message to <a href="http://www.xtc-modified.org/forum/" target="_blank">Forum</a> or to one of the listed developers.');
+define('TEXT_INFO_DISCLAIMER', 'This program was published with the intent to be helpful. Nevertheless, we do not give any warranty that it is without bugs.');
+define('TEXT_INFO_DONATIONS', 'xtcModified is an open source projekt, yet a lot of work and spare time goes into this project and therefore we would be grateful if you show your appreciation by donating to the project.');
+define('TEXT_INFO_DONATIONS_IMG_ALT', 'Please support this project with your donation.');
+?>
