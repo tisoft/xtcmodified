7d03a9aeb79c3fd28ea32410a027916f396f790e
tonne1 <tonne1@25741c12-d480-4641-b65d-acd752be3b7f>
2009-12-04 01:02:44 +0000

    credits internationalization
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@584 25741c12-d480-4641-b65d-acd752be3b7f

---
+++ lang/german/admin/credits.php
@@ -0,0 +1,28 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: credits.php ???? 2009-12-04 01:19:00Z vr $   
+
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
+
+   Copyright (c) 2009 xtcModified
+   --------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(customers.php,v 1.13 2002/06/15); www.oscommerce.com 
+   (c) 2003	 nextcommerce (customers.php,v 1.8 2003/08/15); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Danksagung');
+
+define('TEXT_HEADING_DEVELOPERS', 'Entwickler der xtcModified eCommerce Shopsoftware:');
+define('TEXT_HEADING_SUPPORT', 'Unterstützen Sie die Weiterentwicklung:');
+define('TEXT_HEADING_DONATIONS', 'Spenden:');
+define('TEXT_HEADING_BASED_ON', 'Die Shopsoftware basiert auf:');
+
+define('TEXT_INFO_THANKS', 'Wir danken allen Programmieren und Entwicklern, die an diesem Projekt mitarbeiten. Sollten wir jemanden in der unten stehenden Auflistung vergessen haben, so bitten wir um Mitteilung über das <a href="http://www.xtc-modified.org/forum/" target="_blank">Forum</a> oder an einen der genannten Entwickler.');
+define('TEXT_INFO_DISCLAIMER', 'Dieses Programm wurde ver&ouml;ffentlich, in der Hoffnung hilfreich zu sein. Wir geben jedoch keinerlei Garantie auf die fehlerfreie Implementierung.');
+define('TEXT_INFO_DONATIONS', 'xtcModified ist ein OpenSource-Projekt, trotzden stecken wir jede Menge Arbeit und Freizeit in dieses Projekt und würden uns daher über eine Spende als kleine Anerkennung freuen.');
+?>
