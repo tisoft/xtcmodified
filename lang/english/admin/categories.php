4364591e95e0220df2439b5380c5289b90159069
tonne1 <tonne1@25741c12-d480-4641-b65d-acd752be3b7f>
2009-12-16 22:54:16 +0000

    removed unnecessary defines in language files
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@631 25741c12-d480-4641-b65d-acd752be3b7f

--- lang/english/admin/categories.php
+++ lang/english/admin/categories.php
@@ -55,7 +55,7 @@ define('TEXT_PRODUCT_MORE_INFORMATION', 'For more information, please visit this
 define('TEXT_PRODUCT_DATE_ADDED', 'This product was added to our catalog on %s.');
 define('TEXT_PRODUCT_DATE_AVAILABLE', 'This product will be in stock on %s.');
 // BOF - vr - 2009-12-16 removed unnecessary define
-define('TEXT_CHOOSE_INFO_TEMPLATE', 'Product-Info Template:');
+// define('TEXT_CHOOSE_INFO_TEMPLATE', 'Product-Info Template:');
 // EOF - vr - 2009-12-16 removed unnecessary define
 define('TEXT_CHOOSE_OPTIONS_TEMPLATE', 'Product-Optionen Template:');
 define('TEXT_SELECT', 'Please select:');
@@ -155,7 +155,7 @@ define('HEADING_PRODUCTS_MEDIA','<b>Products Media</b>');
 define('TABLE_HEADING_PRICE','Price');
 
 // BOF - vr - 2009-12-16 removed unnecessary define
-define('TEXT_CHOOSE_INFO_TEMPLATE','Productdetail Template');
+// define('TEXT_CHOOSE_INFO_TEMPLATE','Productdetail Template');
 // EOF - vr - 2009-12-16 removed unnecessary define
 define('TEXT_SELECT','--Select--');
 define('TEXT_CHOOSE_OPTIONS_TEMPLATE','Productoptions Template');
