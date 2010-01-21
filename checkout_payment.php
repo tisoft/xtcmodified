6d63ba037e401c37f25c06e117ab4dc958e04634
jhetfield <jhetfield@25741c12-d480-4641-b65d-acd752be3b7f>
2010-01-21 09:25:40 +0000

    Bugfix including contentfiles at SSL-Proxy
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@703 25741c12-d480-4641-b65d-acd752be3b7f

--- checkout_payment.php
+++ checkout_payment.php
@@ -208,11 +208,10 @@ if (DISPLAY_CONDITIONS_ON_CHECKOUT == 'true') {
 	$shop_content_data = xtc_db_fetch_array($shop_content_query);
 
 	if ($shop_content_data['content_file'] != '') {
-		/* BOF - Hetfield - 2010-01-20 - Bugfix including contentfiles at SSL-Proxy */
+		/* BOF - Hetfield - 2010-01-21 - Bugfix including contentfiles at SSL-Proxy */
 		//$conditions = '<iframe SRC="' . DIR_WS_CATALOG . 'media/content/' . $shop_content_data['content_file'] . '" width="100%" height="300">';
-		$conditions = '<iframe src="' . (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG . 'media/content/' . $shop_content_data['content_file'] . '" width="100%" height="300">';
-		$conditions .= '</iframe>';
-		/* EOF - Hetfield - 2010-01-20 - Bugfix including contentfiles at SSL-Proxy */
+		$conditions = '<div class="agbframe">' . file_get_contents(DIR_FS_DOCUMENT_ROOT . 'media/content/' . $shop_content_data['content_file']) . '</div>';
+		/* EOF - Hetfield - 2010-01-21 - Bugfix including contentfiles at SSL-Proxy */
 	} else {
 		/* BOF - Hetfield - 2010-01-20 - Remove agb-textarea from checkout_payment */
 		//$conditions = '<textarea name="blabla" cols="60" rows="10" readonly="readonly">' . strip_tags(str_replace('<br />', "\n", $shop_content_data['content_text'])) . '</textarea>';
