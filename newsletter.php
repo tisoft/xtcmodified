7dcc43aad5a6f1b4a483cfac4d7bc0aec78c8728
tonne1 <tonne1@25741c12-d480-4641-b65d-acd752be3b7f>
2010-02-07 22:43:44 +0000

    revised newsletter thx to web28
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@752 25741c12-d480-4641-b65d-acd752be3b7f

--- newsletter.php
+++ newsletter.php
@@ -29,8 +29,13 @@ require_once (DIR_FS_INC.'xtc_render_vvcode.inc.php');
 require_once (DIR_FS_INC.'xtc_random_charcode.inc.php');
 require_once (DIR_FS_INC.'xtc_encrypt_password.inc.php');
 require_once (DIR_FS_INC.'xtc_validate_password.inc.php');
+//BOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
+require_once (DIR_FS_INC.'xtc_validate_email.inc.php');
+$inp = 'true';
+//EOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
 
-if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
+if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {    
+	
 	$vlcode = xtc_random_charcode(32);
 	$link = xtc_href_link(FILENAME_NEWSLETTER, 'action=activate&email='.xtc_db_input($_POST['email']).'&key='.$vlcode, 'NONSSL');
 
@@ -53,7 +58,10 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
 
   //BOF - Dokuman - 2009-09-04: convert uppercase Captchas to lowercase, to be more flexible on user input
 	//if (($_POST['check'] == 'inp') && ($_POST['vvcode'] == $_SESSION['vvcode'])) {
-	if (($_POST['check'] == 'inp') && (strtoupper($_POST['vvcode']) == $_SESSION['vvcode'])) {
+  //BOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
+    //if (($_POST['check'] == 'inp') && (strtoupper($_POST['vvcode']) == $_SESSION['vvcode'])) {
+	if (xtc_validate_email(trim($_POST['email'])) && ($_POST['check'] == 'inp') && (strtoupper($_POST['vvcode']) == $_SESSION['vvcode'])) {
+  //BOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
   //EOF - Dokuman - 2009-09-04: convert uppercase Captchas to lowercase, to be more flexible on user input
 
 		$check_mail_query = xtc_db_query("select customers_email_address, mail_status from ".TABLE_NEWSLETTER_RECIPIENTS." where customers_email_address = '".xtc_db_input($_POST['email'])."'");
@@ -109,12 +117,20 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
 		}
 
 	} else {
-		$info_message = TEXT_WRONG_CODE;
+	    //BOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
+	    //$info_message = TEXT_WRONG_CODE;
+	    $info_message = '';
+	    if (!xtc_validate_email(trim($_POST['email']))) $info_message .= ERROR_EMAIL;
+		if (strtoupper($_POST['vvcode']) != $_SESSION['vvcode']) $info_message .= ERROR_VVCODE;		
+		//EOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
 	}
 
   //BOF - Dokuman - 2009-09-04: convert uppercase Captchas to lowercase, to be more flexible on user input
 	//if (($_POST['check'] == 'del') && ($_POST['vvcode'] == $_SESSION['vvcode'])) {
-	if (($_POST['check'] == 'del') && (strtoupper($_POST['vvcode']) == $_SESSION['vvcode'])) {
+  //BOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
+	//if (($_POST['check'] == 'del') && (strtoupper($_POST['vvcode']) == $_SESSION['vvcode'])) {
+	if (xtc_validate_email(trim($_POST['email'])) && ($_POST['check'] == 'del') && (strtoupper($_POST['vvcode']) == $_SESSION['vvcode'])) {
+  //EOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
   //BOF - Dokuman - 2009-09-04: convert uppercase Captchas to lowercase, to be more flexible on user input
 
 		$check_mail_query = xtc_db_query("select customers_email_address from ".TABLE_NEWSLETTER_RECIPIENTS." where customers_email_address = '".xtc_db_input($_POST['email'])."'");
@@ -122,9 +138,12 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
 			$info_message = TEXT_EMAIL_NOT_EXIST;
 		} else {
 			$del_query = xtc_db_query("delete from ".TABLE_NEWSLETTER_RECIPIENTS." where customers_email_address ='".xtc_db_input($_POST['email'])."'");
+            //BOF - web28 - 2010-02-09:UPDATE TABLE CUSTOMERS
+            xtc_db_query("update ".TABLE_CUSTOMERS." set customers_newsletter = '0' where customers_email_address = '".xtc_db_input($_POST['email'])."'");
+			//EOF - web28 - 2010-02-09: UPDATE TABLE CUSTOMERS
 			$info_message = TEXT_EMAIL_DEL;
-		}
-	}
+		}	
+	}	
 }
 
 // Accountaktivierung per Emaillink
@@ -132,12 +151,18 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'activate')) {
 	$check_mail_query = xtc_db_query("select mail_key from ".TABLE_NEWSLETTER_RECIPIENTS." where customers_email_address = '".xtc_db_input($_GET['email'])."'");
 	if (!xtc_db_num_rows($check_mail_query)) {
 		$info_message = TEXT_EMAIL_NOT_EXIST;
-	} else {
+	} else {	    
 		$check_mail = xtc_db_fetch_array($check_mail_query);
-		if (!$check_mail['mail_key'] == $_GET['key']) {
+		//BOF - web28 - 2009-02-09 : FIX WRONG ACTIVATE HANDLING
+		//if (!$check_mail['mail_key'] == $_GET['key']) {
+		if ($check_mail['mail_key'] != $_GET['key']) {
+		//EOF - web28 - 2009-02-09 : FIX WRONG ACTIVATE HANDLING
 			$info_message = TEXT_EMAIL_ACTIVE_ERROR;
 		} else {
 			xtc_db_query("update ".TABLE_NEWSLETTER_RECIPIENTS." set mail_status = '1' where customers_email_address = '".xtc_db_input($_GET['email'])."'");
+			//BOF - web28 - 2010-02-09: UPDATE TABLE CUSTOMERS
+            xtc_db_query("update ".TABLE_CUSTOMERS." set customers_newsletter = '1' where customers_email_address = '".xtc_db_input($_GET['email'])."'");
+			//EOF - web28 - 2010-02-09:UPDATE TABLE CUSTOMERS
 			$info_message = TEXT_EMAIL_ACTIVE;
 		}
 	}
@@ -154,10 +179,16 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'remove')) {
 		$info_message = TEXT_EMAIL_NOT_EXIST;
 	} else {
 		$check_mail = xtc_db_fetch_array($check_mail_query);
-		if (!xtc_validate_password($check_mail['customers_email_address'], $_GET['key'])) {
+		//BOF - web28 - 2009-02-09 : FIX WRONG DEACTIVATE HANDLING
+		//if (!xtc_validate_password($check_mail['customers_email_address'], $_GET['key'])) {
+		if ($check_mail['mail_key'] != $_GET['key']) {
+		//EOF - web28 - 2009-02-09 : FIX WRONG DEACTIVATE HANDLING
 			$info_message = TEXT_EMAIL_DEL_ERROR;
 		} else {
 			$del_query = xtc_db_query("delete from ".TABLE_NEWSLETTER_RECIPIENTS." where  customers_email_address ='".xtc_db_input($_GET['email'])."' and mail_key = '".xtc_db_input($_GET['key'])."'");
+			//BOF - web28 - 2010-02-09: UPDATE TABLE CUSTOMERS
+            xtc_db_query("update ".TABLE_CUSTOMERS." set customers_newsletter = '0' where customers_email_address = '".xtc_db_input($_GET['email'])."'");
+			//EOF - web28 - 2010-02-09: UPDATE TABLE CUSTOMERS
 			$info_message = TEXT_EMAIL_DEL;
 		}
 	}
@@ -175,13 +206,20 @@ $smarty->assign('VVIMG', '<img src="'.xtc_href_link(FILENAME_DISPLAY_VVCODES, 't
 $smarty->assign('text_newsletter', TEXT_NEWSLETTER);
 $smarty->assign('info_message', $info_message);
 $smarty->assign('FORM_ACTION', xtc_draw_form('sign', xtc_href_link(FILENAME_NEWSLETTER, 'action=process', 'NONSSL')));
-$smarty->assign('INPUT_EMAIL', xtc_draw_input_field('email', xtc_db_input($_POST['email'])));
+//BOF - web28 - 2010-02-09: SHOW EMAIL  IN INPUT FIELD
+//$smarty->assign('INPUT_EMAIL', xtc_draw_input_field('email', xtc_db_input($_POST['email'])));
+$smarty->assign('INPUT_EMAIL', xtc_draw_input_field('email', (xtc_db_input($_GET['email']) !='' ? xtc_db_input($_GET['email']) : xtc_db_input($_POST['email']))));
+//EOF - web28 - 2010-02-09: SHOW EMAIL IN INPUT FIELD
 // BOF - Tomcraft - 2010-01-24 - unified the captcha field size.
 //$smarty->assign('INPUT_CODE', xtc_draw_input_field('vvcode', '', 'size="6" maxlength="6"', 'text', false));
 $smarty->assign('INPUT_CODE', xtc_draw_input_field('vvcode', '', 'size="8" maxlength="6"', 'text', false));
 // EOF - Tomcraft - 2010-01-24 - unified the captcha field size.
-$smarty->assign('CHECK_INP', xtc_draw_radio_field('check', 'inp'));
-$smarty->assign('CHECK_DEL', xtc_draw_radio_field('check', 'del'));
+//BOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
+if($_POST['check'] == 'inp') {$inp = 'true'; $del = '';}
+if($_POST['check'] == 'del') {$inp = ''; $del = 'true';}	
+$smarty->assign('CHECK_INP', xtc_draw_radio_field('check', 'inp', $inp));
+//EOF - web28 - 2010-02-09: NEWSLETTER ERROR HANDLING
+$smarty->assign('CHECK_DEL', xtc_draw_radio_field('check', 'del', $del));
 $smarty->assign('BUTTON_SEND', xtc_image_submit('button_send.gif', IMAGE_BUTTON_LOGIN));
 $smarty->assign('FORM_END', '</form>');
 
