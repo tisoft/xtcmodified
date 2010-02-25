8622953fb20d4476ff18e59d4ffe203c21155ca1
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-02-25 19:09:55 +0000

    Added backwards compatibility for older templates that do not include a file /javascript/general.css.php
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@782 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/header.php
+++ includes/header.php
@@ -113,7 +113,12 @@ function popupImageWindow(url) {
 // require theme based javascript
 //BOF - DokuMan - 2010-02-25 - Enhance page loading time by putting CSS on TOP of page and JavaScript on BOTTOM of page
 //require('templates/'.CURRENT_TEMPLATE.'/javascript/general.js.php');
-require('templates/'.CURRENT_TEMPLATE.'/javascript/general.css.php');
+if (file_exists('templates/'.CURRENT_TEMPLATE.'/javascript/general.css.php')) {
+  require('templates/'.CURRENT_TEMPLATE.'/javascript/general.css.php');
+//Backwards compatibility for older templates
+} else {
+  require('templates/'.CURRENT_TEMPLATE.'/javascript/general.js.php');
+}
 //EOF - DokuMan - 2010-02-25 - Enhance page loading time by putting CSS on TOP of page and JavaScript on BOTTOM of page
 
 if (strstr($PHP_SELF, FILENAME_CHECKOUT_PAYMENT)) {
