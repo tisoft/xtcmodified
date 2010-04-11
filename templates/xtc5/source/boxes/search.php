76c648943328e08b15458e324e14befa3ab56c9e
Tomcraft1980 <Tomcraft1980@25741c12-d480-4641-b65d-acd752be3b7f>
2010-04-11 16:00:13 +0000

    Minor change on r875 (enlarge the sreach field to 160px).
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@876 25741c12-d480-4641-b65d-acd752be3b7f

--- templates/xtc5/source/boxes/search.php
+++ templates/xtc5/source/boxes/search.php
@@ -28,7 +28,7 @@ $box_smarty->assign('FORM_ACTION', xtc_draw_form('quick_find', xtc_href_link(FIL
 //$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', '', 'size="20" maxlength="30"'));
 //BOF - web28 - 2010-04-11 - change input html size to css width
 //$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', IMAGE_BUTTON_SEARCH, 'size="20" maxlength="30" onfocus="if(this.value==this.defaultValue) this.value=\'\';" onblur="if(this.value==\'\') this.value=this.defaultValue;"'));
-$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', IMAGE_BUTTON_SEARCH, 'style="width:126px;" maxlength="30" onfocus="if(this.value==this.defaultValue) this.value=\'\';" onblur="if(this.value==\'\') this.value=this.defaultValue;"'));
+$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', IMAGE_BUTTON_SEARCH, 'style="width:160px;" maxlength="30" onfocus="if(this.value==this.defaultValue) this.value=\'\';" onblur="if(this.value==\'\') this.value=this.defaultValue;"'));
 //EOF - web28 - 2010-04-11 - change input html size to css width
 //EOF - Dokuman - 14.08.2009 - Put dynamic "search"-text into box
 $box_smarty->assign('BUTTON_SUBMIT', xtc_image_submit('button_quick_find.gif', IMAGE_BUTTON_SEARCH));
