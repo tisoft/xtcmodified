8cac46a8b66b35e0ca85f7ea03bc60b3fb0ba635
Tomcraft1980 <Tomcraft1980@25741c12-d480-4641-b65d-acd752be3b7f>
2010-05-25 19:35:08 +0000

    Fixed display of error messages from security_check.php in admin area when file and folder-permissions are set correctly.
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@905 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/includes/modules/security_check.php
+++ admin/includes/modules/security_check.php
@@ -53,7 +53,24 @@ if (!strpos(decoct(fileperms(DIR_FS_CATALOG.'media/content/')), '777') and !strp
 	$folder_warning .= '<br />'.DIR_FS_CATALOG.'media/content/';
 }
 
-if ($file_warning != '' or $folder_warning != '') {
+// BOF - Tomcraft - 2010-05-25 - Fixed display of error messages from security_check.php in admin area when file and folder-permissions are set correctly
+//if ($file_warning != '' or $folder_warning != '') {
+	$payment_query = xtc_db_query("SELECT *
+				FROM ".TABLE_CONFIGURATION."
+				WHERE configuration_key = 'MODULE_PAYMENT_INSTALLED'");
+	while ($payment_data = xtc_db_fetch_array($payment_query)) {
+		$installed_payment = $payment_data['configuration_value'];
+
+	}
+	$shipping_query = xtc_db_query("SELECT *
+				FROM ".TABLE_CONFIGURATION."
+				WHERE configuration_key = 'MODULE_SHIPPING_INSTALLED'");
+	while ($shipping_data = xtc_db_fetch_array($shipping_query)) {
+		$installed_shipping = $shipping_data['configuration_value'];
+
+	}
+if ($file_warning != '' or $folder_warning != '' or $installed_payment == '' or $installed_payment == '') {
+// EOF - Tomcraft - 2010-05-25 - Fixed display of error messages from security_check.php in admin area when file and folder-permissions are set correctly
 ?>
 
 
@@ -72,6 +89,8 @@ if ($file_warning != '' or $folder_warning != '') {
             <td class="main">
               <?php
 
+// BOF - Tomcraft - 2010-05-25 - Fixed display of error messages from security_check.php in admin area when file and folder-permissions are set correctly
+/*
 	if ($file_warning != '') {
 
 		echo TEXT_FILE_WARNING;
@@ -106,6 +125,29 @@ if ($file_warning != '' or $folder_warning != '') {
 	if ($installed_shipping == '') {
 		echo '<br />'.TEXT_SHIPPING_ERROR;
 	}
+*/
+	if ($file_warning != '') {
+
+		echo TEXT_FILE_WARNING;
+
+		echo '<strong>'.$file_warning.'</strong><br />';
+	}
+
+	if ($folder_warning != '') {
+
+		echo TEXT_FOLDER_WARNING;
+
+		echo '<strong>'.$folder_warning.'</strong><br />';
+	}
+
+	if ($installed_payment == '') {
+		echo TEXT_PAYMENT_ERROR.'<br />';
+	}
+
+	if ($installed_shipping == '') {
+		echo TEXT_SHIPPING_ERROR;
+	}
+// EOF - Tomcraft - 2010-05-25 - Fixed display of error messages from security_check.php in admin area when file and folder-permissions are set correctly
 ?>
             </td>
           </tr>
