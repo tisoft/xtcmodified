e0ae520bcb9533ec3e270fdff9965761155f80cc
Tomcraft1980 <Tomcraft1980@25741c12-d480-4641-b65d-acd752be3b7f>
2010-06-10 14:33:39 +0000

    Fixed a typo n lang/german/contact_us.php and escaped a german umlaut.
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@947 25741c12-d480-4641-b65d-acd752be3b7f

--- lang/german/contact_us.php
+++ lang/german/contact_us.php
@@ -4,9 +4,9 @@
  define('EMAIL_STREET', 'Strasse: ');
  define('EMAIL_POSTCODE', 'PLZ: ');
  define('EMAIL_CITY', 'Stadt: ');
- define('EMAIL_PHONE', 'Telefone: ');
+ define('EMAIL_PHONE', 'Telefon: ');
  define('EMAIL_FAX', 'Fax: ');
- define('EMAIL_SENT_BY', 'Übermittelt von %s %s am %s um %s Uhr');
+ define('EMAIL_SENT_BY', '&Uuml;bermittelt von %s %s am %s um %s Uhr');
  define('EMAIL_NOTIFY', 'ACHTUNG, diese E-Mail kann NICHT mit -ABSENDER ANTWORTEN- beantwortet werden!');
  
  //default fields
