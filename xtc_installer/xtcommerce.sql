e57627179c73d9874c46991b46991dd3e16f2363
Tomcraft1980 <Tomcraft1980@25741c12-d480-4641-b65d-acd752be3b7f>
2010-06-23 14:47:19 +0000

    Forgot to change database_version in "xtc_installer/xtcommerce.sql" before release of xtcModified 1.04
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@967 25741c12-d480-4641-b65d-acd752be3b7f

--- xtc_installer/xtcommerce.sql
+++ xtc_installer/xtcommerce.sql
@@ -1182,7 +1182,7 @@ DROP TABLE if EXISTS personal_offers_by_customers_status_3;
 
 
 #database Version
-INSERT INTO database_version(version) VALUES ('xtcM_1.0.3.0');
+INSERT INTO database_version(version) VALUES ('xtcM_1.0.4.0');
 
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('0', 'information');
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('1', 'content');
