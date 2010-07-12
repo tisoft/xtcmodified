1824aa5565ad0c3c301786d0a2502774b5922bd1
web28 <web28@25741c12-d480-4641-b65d-acd752be3b7f>
2010-07-12 14:25:11 +0000

    Sort templates array
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1024 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/modules/product_info.php
+++ includes/modules/product_info.php
@@ -215,6 +215,9 @@ if (!is_object($product) || !$product->isProduct()) { // product not found in da
 			} // while
 			closedir($dir);
 		}
+		// BOF - web28 - 2010-07-12 - sort templates array
+		asort($files);
+		// BOF - web28 - 2010-07-12 - sort templates array
 		$product->data['product_template'] = $files[0]['id'];
 	}
 
