bfd94f01fdecc7607b47b30a186e236731f44311
web28 <web28@25741c12-d480-4641-b65d-acd752be3b7f>
2010-07-12 16:17:53 +0000

    Debug sort array on r1024
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1030 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/modules/product_info.php
+++ includes/modules/product_info.php
@@ -217,7 +217,9 @@ if (!is_object($product) || !$product->isProduct()) { // product not found in da
 			} // while
 			closedir($dir);
 		}		
-		sort($files);			
+		sort($files);
+        echo print_r($files);
+		echo $files[0];		
 		//$product->data['product_template'] = $files[0]['id'];
 		$product->data['product_template'] = $files[0];
 // BOF - web28 - 2010-07-12 - sort templates array
