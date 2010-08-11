1402d9af5675a14f12a9b187ffefbb4572ec139a
Tomcraft1980 <Tomcraft1980@25741c12-d480-4641-b65d-acd752be3b7f>
2010-08-11 11:39:15 +0000

    Added missing changes to update_1.0.5.0_to_1.0.6.0.sql on r1134.
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1135 25741c12-d480-4641-b65d-acd752be3b7f

--- xtc_installer/update_1.0.5.0_to_1.0.6.0.sql
+++ xtc_installer/update_1.0.5.0_to_1.0.6.0.sql
@@ -3,3 +3,6 @@ UPDATE database_version SET version = 'xtcM_1.0.6.0';
 
 #DokuMan - 2010-08-05 - mark out of stock products red by default
 UPDATE configuration SET configuration_value = 'span style="color:red">***</span>' WHERE configuration_key = 'STOCK_MARK_PRODUCT_OUT_OF_STOCK';
+
+#Hendrik - 2010-08-11 - Thumbnails in products list 
+INSERT INTO configuration (configuration_id, configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES   ('', 'USE_ADMIN_THUMBS_IN_LIST', 'true', 1, 32, NULL , NOW( ) , NULL , 'xtc_cfg_select_option(array(\'true\', \'false\'),'); 
