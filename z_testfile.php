e0213069b90efa5d75bfb7f85ceb6e6451740743
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-08-13 19:52:51 +0000

    dokuman testfile
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1156 25741c12-d480-4641-b65d-acd752be3b7f

---
+++ z_testfile.php
@@ -0,0 +1,22 @@
+<?php
+
+/* -----------------------------------------------------------------------------------------
+   $Id$   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(ssl_check.php,v 1.1 2003/03/10); www.oscommerce.com 
+   (c) 2003	 nextcommerce (ssl_check.php,v 1.9 2003/08/17); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+include ('includes/application_top.php');
+
+include ('includes/application_bottom.php');
+?>
