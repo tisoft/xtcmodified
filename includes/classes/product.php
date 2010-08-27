f2fd94c925b87772b2bc5caec8ab37d927139c31
gtb-modified <gtb-modified@25741c12-d480-4641-b65d-acd752be3b7f>
2010-08-27 08:37:07 +0000

    set Button Products Details global
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1218 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/classes/product.php
+++ includes/classes/product.php
@@ -471,7 +471,7 @@ class product {
 				'PRODUCTS_SHIPPING_NAME'=>$shipping_status_name,
 				'PRODUCTS_SHIPPING_IMAGE'=>$shipping_status_image, 
 				 //BOF - GTB - 2010-08-27 make Button Details global
-				'PRODUCTS_BUTTON_DETAILS' = '<a href="'.xtc_href_link(FILENAME_PRODUCT_INFO, xtc_product_link($array['products_id'], $array['products_name'])).'">'.xtc_image_button('button_product_more.gif', $array['products_name'].TEXT_NOW).'</a>',
+				'PRODUCTS_BUTTON_DETAILS' => '<a href="'.xtc_href_link(FILENAME_PRODUCT_INFO, xtc_product_link($array['products_id'], $array['products_name'])).'">'.xtc_image_button('button_product_more.gif', $array['products_name'].TEXT_NOW).'</a>',
 				 //EOF - GTB - 2010-08-27 make Button Details global
 				 
 				//'PRODUCTS_DESCRIPTION' => $array['products_description'],
