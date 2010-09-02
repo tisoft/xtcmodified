9dd71f71cc3f9d4faab0e1dc00706de4ae042156
jhetfield <jhetfield@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-02 07:09:47 +0000

    New xtc_show_category.inc.php for correctly ul-Lists and active-Links
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1254 25741c12-d480-4641-b65d-acd752be3b7f

--- templates/xtc5/source/inc/xtc_show_category.inc.php
+++ templates/xtc5/source/inc/xtc_show_category.inc.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: xtc_show_category.inc.php 1262 2005-09-30 10:00:32Z mz $
+   $Id: xtc_show_category.inc.php,v 1.5 2008/04/11 15:30:00 Hetfield $
 
    XT-Commerce - community made shopping
    http://www.xt-commerce.com
@@ -11,67 +11,78 @@
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(categories.php,v 1.23 2002/11/12); www.oscommerce.com
    (c) 2003	 nextcommerce (xtc_show_category.inc.php,v 1.4 2003/08/13); www.nextcommerce.org 
+   (c) 2008 Hetfield (xtc_show_category.inc.php,v 1.5 2008/04/11); www.merz-it-service.de
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
-function xtc_show_category($counter) {
-	global $foo, $categories_string, $id;
-
-    // image for first level
-    //$img_1='<img src="templates/'.CURRENT_TEMPLATE.'/img/icon_arrow.jpg" alt="" />&nbsp;';
-
-	if ($foo[$counter]['level']=='') {
-		if (strlen($categories_string)=='0') {
-			$categories_string .='';
-		} else {
-			//$categories_string .= '<li class="submenuspacer"></li>';
-			$categories_string .= '';
-			$categories_string .='';
-		}
-		if (trim($foo[$counter]['name']) != '' ) $categories_string .= '<li class="level1"><a href="';
+function xtc_show_category($counter,$oldlevel='1',$last_next_id = '',$ul_open = false) {
+    global $foo, $categories_string, $id;
+		
+	$category_path = explode('_',$GLOBALS['cPath']); 
+	$in_path = in_array($foo[$counter]['path'], $category_path);
+	$this_category = array_pop($category_path);
+	$einzug = '';
+	
+	for ($a=0; $a<$foo[$counter]['level']; $a++) {
+		$einzug .= "\t";
+	}
+ 	$categories_string .= '';
+ 	if ( ($id) && (in_array($counter, $id)) ) {
+      	$submenu = " childs";
+    } else {
+		$submenu = "";
+	}
+	if ($in_path) { 
+		$active = " activeparent";
+		$active2 = " class=\"actparentlink\"";
 	} else {
-		if (trim($foo[$counter]['name']) != '' ) $categories_string .= '<li class="level'.($foo[$counter]['level']+1).'"><a  href="';
+		$active = "";
+		$active2 = "";
 	}
-
-	$cPath_new=xtc_category_link($counter,$foo[$counter]['name']);
-    
-	if (trim($foo[$counter]['name']) != '' ) {
-		$categories_string .= xtc_href_link(FILENAME_DEFAULT, $cPath_new);
-		$categories_string .= '">';
-    }
-	
-	/* Anzeigen der Kategoriebezeichnung
-	if ($foo[$counter]['level']=='1') {
-		$categories_string .= '';
-	}*/
-	
-	$categories_string .= $foo[$counter]['name'];
-
-	if ( ($id) && (in_array($counter, $id)) ) {
-			//$categories_string .= '</b>';
+	if ($this_category == $counter) {
+		$current = " active";
+		$current2 = " class=\"actlink\"";
+	} else {
+		$current = "";
+		$current2 = "";
 	}
-
+	if ($oldlevel > $foo[$counter]['level'] and $last_next_id != '') {
+		$categories_string .= "</li>"."\n\t".$einzug."</ul>"."\n\t".$einzug."</li>\n".$einzug;
+	} else if ($ul_open == true) {
+		$categories_string .= "\n".$einzug;
+	} else if ($last_next_id != '') {
+		$categories_string .= "</li>\n".$einzug;
+	}
+	if ($einzug != '') {
+		$einzug2 = $einzug;
+	} else {
+		$einzug2 = "\t";
+	}
+    $categories_string .= $einzug2.'<li class="level'.($foo[$counter]['level']+1).$active.$current.$submenu.'">';
+	$categories_string .= "<a".$current2.$active2." href=\"".xtc_href_link(FILENAME_DEFAULT, xtc_category_link($counter,$foo[$counter]['name']))."\" title=\"".$foo[$counter]['name']."\">";
+    $categories_string .= $foo[$counter]['name']."</a>";    
 	if (SHOW_COUNTS == 'true') {
-		$products_in_category = xtc_count_products_in_category($counter);
-		if ($products_in_category > 0) {
-			if (trim($foo[$counter]['name']) != '' ) $categories_string .= '&nbsp;(' . $products_in_category . ')';
-		}
-	}
-
-	if ($foo[$counter]['level']=='') {
-		if (trim($foo[$counter]['name']) != '' ) $categories_string .= '</a></li>';
+      	$products_in_category = xtc_count_products_in_category($counter);
+      	if ($products_in_category > 0) {
+        	$categories_string .= "&nbsp;<em>(" . $products_in_category . ")</em>";
+      	}
+    }	
+	if ($counter == get_parent($foo[$counter]['next_id'])) {
+		$categories_string .= "\n\t".$einzug."<ul>";
+		$ul_open = true;
 	} else {
-		if (trim($foo[$counter]['name']) != '' ) $categories_string .= '</a></li>';
-		if ($foo[$counter]['level']=='1') {
-			$categories_string .='';
-		}
+		$ul_open = false;
 	}
-
-	if ($foo[$counter]['next_id']) {
-		xtc_show_category($foo[$counter]['next_id']);
-	} else {
-		$categories_string .= '';
-	}
+	$categories_string .= "";
+    if ($foo[$counter]['next_id']) {
+      	xtc_show_category($foo[$counter]['next_id'],$foo[$counter]['level'],$foo[$counter]['next_id'],$ul_open);
+    }
+}
+function get_parent($categories_id) {
+    $parent_query = "SELECT parent_id FROM " . TABLE_CATEGORIES . " WHERE categories_id = '" . $categories_id . "'";
+    $parent_query  = xtDBquery($parent_query);
+    $parent = xtc_db_fetch_array($parent_query);
+	return $parent['parent_id'];
 }
 ?>
