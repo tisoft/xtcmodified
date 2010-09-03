2f1e6dd491f58460e09b82a3a9869aa4885c79ec
gtb-modified <gtb-modified@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-03 12:37:23 +0000

    Security Fix for Base
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1263 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/coupon_admin.php
+++ admin/coupon_admin.php
@@ -75,7 +75,10 @@
       $smarty->compile_dir=DIR_FS_CATALOG.'templates_c';
       $smarty->config_dir=DIR_FS_CATALOG.'lang';
 
-      $smarty->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/');
+      //BOF - GTB - 2010-08-03 - Security Fix - Base
+	  $smarty->assign('tpl_path',DIR_WS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/');
+	  //$smarty->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/');
+      //EOF - GTB - 2010-08-03 - Security Fix - Base
       $smarty->assign('logo_path',HTTP_SERVER  . DIR_WS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/img/');
 
       $smarty->assign('MESSAGE', $_POST['message']);
