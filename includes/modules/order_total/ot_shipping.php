e3c72bc5688a71b4af2cef747d98a9bd83c09f2f
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-06 14:17:41 +0000

    Trying to get property of non-object, Undefined index: inkl. Unbekannter Steuersatz
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1273 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/modules/order_total/ot_shipping.php
+++ includes/modules/order_total/ot_shipping.php
@@ -1,20 +1,21 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: ot_shipping.php 1002 2005-07-10 16:11:37Z mz $   
+   $Id$
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
-   based on: 
+   based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(ot_shipping.php,v 1.15 2003/02/07); www.oscommerce.com 
+   (c) 2002-2003 osCommerce(ot_shipping.php,v 1.15 2003/02/07); www.oscommerce.com
    (c) 2003	 nextcommerce (ot_shipping.php,v 1.13 2003/08/24); www.nextcommerce.org
+   (c) 2006 xt:Commerce (ot_shipping.php 1002 2005-07-10); www.xt-commerce.de
 
-   Released under the GNU General Public License 
+   Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
-   
+
   class ot_shipping {
     var $title, $output;
 
@@ -25,8 +26,6 @@
       $this->description = MODULE_ORDER_TOTAL_SHIPPING_DESCRIPTION;
       $this->enabled = ((MODULE_ORDER_TOTAL_SHIPPING_STATUS == 'true') ? true : false);
       $this->sort_order = MODULE_ORDER_TOTAL_SHIPPING_SORT_ORDER;
-
-
       $this->output = array();
     }
 
@@ -57,28 +56,47 @@
       if (xtc_not_null($order->info['shipping_method'])) {
         if ($_SESSION['customers_status']['customers_status_show_price_tax'] == 1) {
         // price with tax
-
-          $shipping_tax = xtc_get_tax_rate($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
-          $shipping_tax_description = xtc_get_tax_description($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+          //BOF - DokuMan - 2010-09-06 - Trying to get property of non-object
+          $tax_class = 0;
+          if (isset($GLOBALS[$module]->tax_class) && isset($GLOBALS[$module]->tax_class)) {
+            $tax_class = $GLOBALS[$module]->tax_class;
+          }
+          $shipping_tax = xtc_get_tax_rate($tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+          $shipping_tax_description = xtc_get_tax_description($tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+          //$shipping_tax = xtc_get_tax_rate($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+          //$shipping_tax_description = xtc_get_tax_description($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+          //EOF - DokuMan - 2010-09-06 - Trying to get property of non-object
           $tax = $xtPrice->xtcFormat(xtc_add_tax($order->info['shipping_cost'], $shipping_tax),false,0,false)-$order->info['shipping_cost'];
           $tax = $xtPrice->xtcFormat($tax,false,0,true);
           $order->info['shipping_cost'] = xtc_add_tax($order->info['shipping_cost'], $shipping_tax);
           $order->info['tax'] += $tax;
-          $order->info['tax_groups'][TAX_ADD_TAX . "$shipping_tax_description"] += $tax;
+          //BOF - DokuMan - 2010-09-06 - Undefined index: inkl. Unbekannter Steuersatz
+          if (!isset($order->info['tax_groups'][TAX_ADD_TAX . $shipping_tax_description])) $order->info['tax_groups'][TAX_ADD_TAX . $shipping_tax_description] = 0;
+          //EOF - DokuMan - 2010-09-06 - Undefined index: inkl. Unbekannter Steuersatz
+          $order->info['tax_groups'][TAX_ADD_TAX . $shipping_tax_description] += $tax;
           $order->info['total'] += $tax;
 
         } else {
-
-        if ($_SESSION['customers_status']['customers_status_show_price_tax'] == 0 && $_SESSION['customers_status']['customers_status_add_tax_ot'] == 1) {
-
-          $shipping_tax = xtc_get_tax_rate($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
-          $shipping_tax_description = xtc_get_tax_description($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
-          $tax = $xtPrice->xtcFormat(xtc_add_tax($order->info['shipping_cost'], $shipping_tax),false,0,false)-$order->info['shipping_cost'];
-		 $tax = $xtPrice->xtcFormat($tax,false,0,true);
-
-          $order->info['tax'] = $order->info['tax'] += $tax;
-          $order->info['tax_groups'][TAX_NO_TAX . "$shipping_tax_description"] = $order->info['tax_groups'][TAX_NO_TAX . "$shipping_tax_description"] += $tax;
-        }
+	        if ($_SESSION['customers_status']['customers_status_show_price_tax'] == 0 && $_SESSION['customers_status']['customers_status_add_tax_ot'] == 1) {
+            //BOF - DokuMan - 2010-09-06 - Trying to get property of non-object
+	          $tax_class = 0;
+	          if (isset($GLOBALS[$module]->tax_class) && isset($GLOBALS[$module]->tax_class)) {
+	            $tax_class = $GLOBALS[$module]->tax_class;
+	          }
+	          $shipping_tax = xtc_get_tax_rate($tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+	          $shipping_tax_description = xtc_get_tax_description($tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+	          //$shipping_tax = xtc_get_tax_rate($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+	          //$shipping_tax_description = xtc_get_tax_description($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+	          //EOF - DokuMan - 2010-09-06 - Trying to get property of non-object
+	          $tax = $xtPrice->xtcFormat(xtc_add_tax($order->info['shipping_cost'], $shipping_tax),false,0,false)-$order->info['shipping_cost'];
+	          $tax = $xtPrice->xtcFormat($tax,false,0,true);
+	          $order->info['tax'] = $order->info['tax'] += $tax;
+	          //BOF - DokuMan - 2010-09-06 - Undefined index: inkl. Unbekannter Steuersatz
+	          if (!isset($order->info['tax_groups'][TAX_NO_TAX . $shipping_tax_description])) $order->info['tax_groups'][TAX_NO_TAX . $shipping_tax_description] = 0;
+	          $order->info['tax_groups'][TAX_NO_TAX . $shipping_tax_description] += $tax;
+	          //$order->info['tax_groups'][TAX_NO_TAX . $shipping_tax_description] = $order->info['tax_groups'][TAX_NO_TAX . "$shipping_tax_description"] += $tax;
+	          //EOF - DokuMan - 2010-09-06 - Undefined index: inkl. Unbekannter Steuersatz
+	        }
         }
         $this->output[] = array('title' => $order->info['shipping_method'] . ':',
                                 'text' => $xtPrice->xtcFormat($order->info['shipping_cost'], true,0,true),
@@ -105,7 +123,7 @@
       xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) values ('MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING', 'false','6', '3', 'xtc_cfg_select_option(array(\'true\', \'false\'), ', now())");
       xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, use_function, date_added) values ('MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING_OVER', '50', '6', '4', 'currencies->format', now())");
       xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) values ('MODULE_ORDER_TOTAL_SHIPPING_DESTINATION', 'national','6', '5', 'xtc_cfg_select_option(array(\'national\', \'international\', \'both\'), ', now())");
-      xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, use_function, set_function, date_added) values ('MODULE_ORDER_TOTAL_SHIPPING_TAX_CLASS', '0','6', '7', 'xtc_get_tax_class_title', 'xtc_cfg_pull_down_tax_classes(', now())");      
+      xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, use_function, set_function, date_added) values ('MODULE_ORDER_TOTAL_SHIPPING_TAX_CLASS', '0','6', '7', 'xtc_get_tax_class_title', 'xtc_cfg_pull_down_tax_classes(', now())");
     }
 
     function remove() {
