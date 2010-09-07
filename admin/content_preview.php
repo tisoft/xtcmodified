b768013467a55686b4f3108033425bbe17c0e727
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-07 11:24:27 +0000

    Missing body-tag
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1280 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/content_preview.php
+++ admin/content_preview.php
@@ -1,20 +1,20 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: content_preview.php 1304 2005-10-12 18:04:43Z mz $   
+   $Id$
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
    based on:
    (c) 2003	 nextcommerce (content_preview.php,v 1.2 2003/08/25); www.nextcommerce.org
-   
-   Released under the GNU General Public License 
-   ---------------------------------------------------------------------------------------*/
-   
-require('includes/application_top.php');
+   (c) 2006 XT-Commerce (coupon_admin.php 1084 2005-07-23)
 
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+require('includes/application_top.php');
 
 if ($_GET['pID']=='media') {
 	$content_query=xtc_db_query("SELECT
@@ -24,7 +24,7 @@ if ($_GET['pID']=='media') {
  					FROM ".TABLE_PRODUCTS_CONTENT."
  					WHERE content_id='".(int)$_GET['coID']."'");
  	$content_data=xtc_db_fetch_array($content_query);
-	
+
 } else {
 	 $content_query=xtc_db_query("SELECT
  					content_title,
@@ -42,25 +42,27 @@ if ($_GET['pID']=='media') {
 <title><?php echo $page_title; ?></title>
 <link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
 </head>
-<div class="pageHeading"><?php echo $content_data['content_heading']; ?></div><br />
+<body>
+<div class="pageHeading"><?php echo $content_data['content_heading']; ?></div>
+<br />
 <table border="0" width="100%" cellspacing="0" cellpadding="2">
           <tr>
             <td class="main">
- <?php
- if ($content_data['content_file']!=''){
-if (strpos($content_data['content_file'],'.txt')) echo '<pre>';
-if ($_GET['pID']=='media') {
-	// display image
-	if (preg_match('/.gif/i',$content_data['content_file']) or preg_match('/.jpg/i',$content_data['content_file']) or  preg_match('/.png/i',$content_data['content_file']) or  preg_match('/.tif/i',$content_data['content_file']) or  preg_match('/.bmp/i',$content_data['content_file'])) { // Hetfield - 2009-08-19 - replaced deprecated function eregi with preg_match to be ready for PHP >= 5.3	
-	echo xtc_image(DIR_WS_CATALOG.'media/products/'.$content_data['content_file']);
-	} else {
-	include(DIR_FS_CATALOG.'media/products/'.$content_data['content_file']);	
-	}
-} else {
-include(DIR_FS_CATALOG.'media/content/'.$content_data['content_file']);	
-}
-if (strpos($content_data['content_file'],'.txt')) echo '</pre>';
- } else {	      
+<?php
+if ($content_data['content_file']!=''){
+  if (strpos($content_data['content_file'],'.txt')) echo '<pre>';
+  if ($_GET['pID']=='media') {
+    // display image
+    if (preg_match('/.gif/i',$content_data['content_file']) or preg_match('/.jpg/i',$content_data['content_file']) or  preg_match('/.png/i',$content_data['content_file']) or  preg_match('/.tif/i',$content_data['content_file']) or  preg_match('/.bmp/i',$content_data['content_file'])) { // Hetfield - 2009-08-19 - replaced deprecated function eregi with preg_match to be ready for PHP >= 5.3
+    echo xtc_image(DIR_WS_CATALOG.'media/products/'.$content_data['content_file']);
+    } else {
+      include(DIR_FS_CATALOG.'media/products/'.$content_data['content_file']);
+    }
+  } else {
+  include(DIR_FS_CATALOG.'media/content/'.$content_data['content_file']);
+  }
+  if (strpos($content_data['content_file'],'.txt')) echo '</pre>';
+ } else {
 echo $content_data['content_text'];
 }
 ?>
