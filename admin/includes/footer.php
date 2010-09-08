49b3998081ae827b1fb12783cea11cc964844664
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-08 10:00:24 +0000

    Changed copyright information to xtcModified
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1286 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,16 +1,17 @@
 <?php
 /* --------------------------------------------------------------
-   $Id: footer.php 899 2005-04-29 02:40:57Z hhgag $   
+   $Id$   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce
-   --------------------------------------------------------------
+   Copyright (c) 2010 xtcModified
+   -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(footer.php,v 1.12 2003/02/17); www.oscommerce.com 
    (c) 2003	 nextcommerce (footer.php,v 1.11 2003/08/18); www.nextcommerce.org
+   (c) 2006 XT-Commerce (footer.php 899 2005-04-29)
 
    Released under the GNU General Public License 
    --------------------------------------------------------------*/
@@ -31,14 +32,12 @@
   Copyright announcement changed due to the permissions
   from LG Hamburg from 28th February 2003 / AZ 308 O 70/03
 */
-?>E-Commerce Engine Copyright &copy; 2006 <a href="http://www.xt-commerce.com" target="_blank">xt:Commerce</a><br />
-xt:Commerce provides no warranty and is redistributable under the <a href="http://www.fsf.org/licenses/gpl.txt" target="_blank">GNU General Public License</a></td>
+?>xtcModified &copy; 2009-<?php echo date("Y"); ?> <a href="http://www.xtc-modified.org" target="_blank">xtcModified</a>
+<br />based on E-Commerce Engine Copyright &copy; 2006 <a href="http://www.xt-commerce.com" target="_blank">xt:Commerce</a>
+<br />xtcModified provides no warranty and is redistributable under the <a href="http://www.fsf.org/licenses/gpl.txt" target="_blank">GNU General Public License</a></td>
   </tr>
   <tr>
     <td><?php echo xtc_image(DIR_WS_IMAGES . 'pixel_trans.gif', '', '1', '5'); ?></td>
-  </tr>
-  <tr>
-    <td align="center" class="smallText">Powered by <a href="http://www.xt-commerce.com" target="_blank">xt:Commerce</a></td>
   </tr>
 </table>
 <?php
@@ -49,6 +48,5 @@ xt:Commerce provides no warranty and is redistributable under the <a href="http:
   echo "</pre>";
   echo '</font>';
   echo xtc_session_id();
-
 */
 ?>
