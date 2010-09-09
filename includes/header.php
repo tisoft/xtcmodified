bc26ebe219dfcd0b9aff4ab74f07a0990dad8c5e
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-09 11:01:53 +0000

    Specify character set early (according to Google's page speed suggestions) to speed up browser rendering
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1293 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/header.php
+++ includes/header.php
@@ -30,9 +30,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php echo HTML_PARAMS; ?>>
 <head>
-<?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>" />
+<meta http-equiv="Content-Type" content="text/html;charset=<?php echo $_SESSION['language_charset']; ?>" />
 <meta http-equiv="Content-Style-Type" content="text/css" />
+<?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
 <link rel="shortcut icon" href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER).DIR_WS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/favicon.ico';?>" type="image/x-icon" />
 
 <?php
