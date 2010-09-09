daa3281771eb0e14162f82ac60f9d90c3e490f69
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-09 09:35:48 +0000

    Security Fix - Base, used xtc_image_button for correct image dimensions
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1292 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_image_button.inc.php
+++ inc/xtc_image_button.inc.php
@@ -2,22 +2,25 @@
 /* -----------------------------------------------------------------------------------------
    $Id: xtc_image_button.inc.php 899 2005-04-29 02:40:57Z hhgag $   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(html_output.php,v 1.52 2003/03/19); www.oscommerce.com 
    (c) 2003	 nextcommerce (xtc_image_button.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2006 XT-Commerce
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
    
 // Output a function button in the selected language
   function xtc_image_button($image, $alt = '', $parameters = '') {
-
-    return xtc_image('templates/'.CURRENT_TEMPLATE.'/buttons/' . $_SESSION['language'] . '/'. $image, $alt, '', '', $parameters);
+    //BOF - DokuMan - 2010-09-09 - Security Fix - Base
+    //return xtc_image('templates/'.CURRENT_TEMPLATE.'/buttons/' . $_SESSION['language'] . '/'. $image, $alt, '', '', $parameters);
+    return xtc_image(DIR_WS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/buttons/' . $_SESSION['language'] . '/'. $image, $alt, '', '', $parameters);
+    //BOF - DokuMan - 2010-09-09 - Security Fix - Base
   }
- ?>
+?>
