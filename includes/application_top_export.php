7e81d02d7495e836ea4b7219c70aeed59f02d7f9
web28 <web28@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-15 21:51:29 +0000

    Security Fix for BASE Tag SSL Proxy
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1317 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/application_top_export.php
+++ includes/application_top_export.php
@@ -61,7 +61,7 @@ if (file_exists('includes/request_type.php')) {
 
 //BOF - GTB/web28 - 2010-09-15 - Security Fix - Base
 $ssl_proxy = '';
-if ($request_type == 'SSL' && ENABLE_SSL == true && USE_SSL_PROXY == true) $ssl_proxy = '/' . $_SERVER['HTTP_HOST'];
+if ($request_type == 'SSL' && ENABLE_SSL == true && defined(USE_SSL_PROXY) && USE_SSL_PROXY == true) $ssl_proxy = '/' . $_SERVER['HTTP_HOST'];
 define('DIR_WS_BASE', $ssl_proxy . preg_replace('/\\' . DIRECTORY_SEPARATOR . '\/|\/\//', '/', dirname($PHP_SELF) . '/'));
 //EOF - GTB/web28 - 2010-09-15 - Security Fix - Base
 
