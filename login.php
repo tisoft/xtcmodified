49be5c2d451cd31f4056e648ea68b99e222f8d07
web28 <web28@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-21 14:09:17 +0000

    redirect to shoppincart NONSSL
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1364 25741c12-d480-4641-b65d-acd752be3b7f

--- login.php
+++ login.php
@@ -93,7 +93,10 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
 			if (isset($econda) && is_object($econda)) $econda->_loginUser();
 
 			if ($_SESSION['cart']->count_contents() > 0) {
-				xtc_redirect(xtc_href_link(FILENAME_SHOPPING_CART, '', 'SSL'));
+				//BOF - web28 - 2010-09-20 redirect NONSSL
+				//xtc_redirect(xtc_href_link(FILENAME_SHOPPING_CART, '', 'SSL'));
+				xtc_redirect(xtc_href_link(FILENAME_SHOPPING_CART), 'NONSSL');
+				//EOF - web28 - 2010-09-20 redirect NONSSL
 			} else {
 			    //BOF - web28 - 2010-08-11 redirect NONSSL
 				//xtc_redirect(xtc_href_link(FILENAME_DEFAULT));
