c053bc0e474808327cc0d32a0075a82687e4a16b
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-09-30 08:44:47 +0000

    support for TLD like ".co.uk", see http://www.xtc-modified.org/forum/topic.php?id=7564
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1389 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_get_top_level_domain.inc.php
+++ inc/xtc_get_top_level_domain.inc.php
@@ -2,15 +2,16 @@
 /* -----------------------------------------------------------------------------------------
    $Id: xtc_get_top_level_domain.inc.php 1535 2006-08-20 21:38:20Z mz $   
 
-   xt:Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2006 xt:Commerce
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(general.php,v 1.225 2003/05/29); www.oscommerce.com 
    (c) 2003	 nextcommerce (xtc_get_top_level_domain.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2006 XT-Commerce (xtc_get_top_level_domain.inc.php 1535 2006-08-20)
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
@@ -24,18 +25,12 @@ function xtc_get_top_level_domain($url) {
 	if ($domain_size > 1) {
 		if (is_numeric($domain_array[$domain_size -2]) && is_numeric($domain_array[$domain_size -1])) {
 			return false;
+    //BOF - DokuMan - 2010-09-30 - support for TLD like ".co.uk"
+    } elseif ($domain_size > 3) {
+			return $domain_array[$domain_size - 3] . '.' . $domain_array[$domain_size - 2] . '.' . $domain_array[$domain_size - 1];
 		} else {
-    //BOF - Dokuman - 2009-09-16 - Fix forced session-ID due to not correctly determined TLD
-      /*
-			for ($domain_part = 1; $domain_part < $domain_size; $domain_part++) {
-				$domain_path .= $domain_array[$domain_part];
-				if ($domain_part != ($domain_size -1))
-					$domain_path .= '.';
-			}
-			return $domain_path;
-      */
 			return $domain_array[$domain_size - 2] . '.' . $domain_array[$domain_size - 1];
-    //EOF - Dokuman - 2009-09-16 - Fix forced session-ID due to not correctly determined TLD
+    //EOF - DokuMan - 2010-09-30 - support for TLD like ".co.uk"
 		}
 	} else {
 		return false;
