5d67400358fdf77d58207cf03f2d96992d1060ad
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-10-29 11:26:55 +0000

    Admin currency updater with cURL-fallback option, if "file()" is disabled.
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1434 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/includes/functions/localization.php
+++ admin/includes/functions/localization.php
@@ -1,45 +1,108 @@
 <?php
 /* --------------------------------------------------------------
-   $Id: localization.php 950 2005-05-14 16:45:21Z mz $
+   $Id$
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2010 xtcModified
    --------------------------------------------------------------
    based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(localization.php,v 1.12 2003/06/25); www.oscommerce.com
-   (c) 2003	 nextcommerce (localization.php,v 1.4 2003/08/14); www.nextcommerce.org
+   (c) 2003	nextcommerce (localization.php,v 1.4 2003/08/14); www.nextcommerce.org
+   (c) 2005 XT-Commerce (sessions.php 950 2005-05-14)
+   (c) 2006 The zen-cart developers http://www.zen-cart.com/index.php
 
    Released under the GNU General Public License
    --------------------------------------------------------------*/
 defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
+
   function quote_oanda_currency($code, $base = DEFAULT_CURRENCY) {
-    $page = file('http://www.oanda.com/convert/fxdaily?value=1&redirected=1&exch=' . $code .  '&format=CSV&dest=Get+Table&sel_list=' . $base);
+    $url = 'http://www.oanda.com/convert/fxdaily';
+    $data = 'value=1&redirected=1&exch=' . $code .  '&format=CSV&dest=Get+Table&sel_list=' . $base;
+    // check via file() ... may fail if php file Wrapper disabled.
+    $page = @file($url . '?' . $data);
+    if (!is_object($page) && function_exists('curl_init')) {
+      // check via cURL instead.  May fail if proxy not set, esp with GoDaddy.
+      $page = doCurlCurrencyRequest('POST', $url, $data) ;
+      $page = explode("\n", $page);
+    }
+    if (is_object($page) || $page !='') {
+      $match = array();
 
-    $match = array();
+      preg_match('/(.+),(\w{3}),([0-9.]+),([0-9.]+)/i', implode('', $page), $match);
 
-    preg_match('/(.+),(\w{3}),([0-9.]+),([0-9.]+)/i', implode('', $page), $match);
-
-    if (sizeof($match) > 0) {
-      return $match[3];
-    } else {
-      return false;
+      if (sizeof($match) > 0) {
+        return $match[3];
+      } else {
+        return false;
+      }
     }
   }
 
   function quote_xe_currency($to, $from = DEFAULT_CURRENCY) {
-    $page = file('http://www.xe.net/ucc/convert.cgi?Amount=1&From=' . $from . '&To=' . $to);
+    $url = 'http://www.xe.net/ucc/convert.cgi';
+    $data = 'Amount=1&From=' . $from . '&To=' . $to;
+    // check via file() ... may fail if php file Wrapper disabled.
+    $page = @file($url . '?' . $data);
+    if (!is_object($page) && function_exists('curl_init')) {
+      // check via cURL instead.  May fail if proxy not set, esp with GoDaddy.
+      $page = doCurlCurrencyRequest('POST', $url, $data) ;
+      $page = explode("\n", $page);
+    }
+    if (is_object($page) || $page !='') {
+      $match = array();
 
-    $match = array();
-
-    preg_match('/[0-9.]+\s*' . $from . '\s*=\s*([0-9.]+)\s*' . $to . '/', implode('', $page), $match);
-
-    if (sizeof($match) > 0) {
-      return $match[1];
-    } else {
-      return false;
+      preg_match('/[0-9.]+\s*' . $from . '\s*=\s*([0-9.]+)\s*' . $to . '/', implode('', $page), $match);
+      if (sizeof($match) > 0) {
+        return $match[1];
+      } else {
+        return false;
+      }
     }
   }
+
+  function doCurlCurrencyRequest($method, $url, $vars) {
+    //echo '-----------------<br />';
+    //echo 'URL: ' . $url . ' VARS: ' . $vars . '<br />';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_VERBOSE, 0);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//  curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+//  curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
+    if (strtoupper($method) == 'POST') {
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $vars);
+    }
+    if (CURL_PROXY_REQUIRED == 'True') {
+      $proxy_tunnel_flag = (defined('CURL_PROXY_TUNNEL_FLAG') && strtoupper(CURL_PROXY_TUNNEL_FLAG) == 'FALSE') ? false : true;
+      curl_setopt ($ch, CURLOPT_HTTPPROXYTUNNEL, $proxy_tunnel_flag);
+      curl_setopt ($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+      curl_setopt ($ch, CURLOPT_PROXY, CURL_PROXY_SERVER_DETAILS);
+    }
+    $data = curl_exec($ch);
+    $error = curl_error($ch);
+    //$info=curl_getinfo($ch);
+    curl_close($ch);
+
+    if ($error != '') {
+      global $messageStack;
+      $messageStack->add_session('cURL communication ERROR: ' . $error, 'error');
+    }
+    //echo 'INFO: <pre>'; print_r($info); echo '</pre><br />';
+    //echo 'ERROR: ' . $error . '<br />';
+    //print_r($data) ;
+
+    if ($data != '') {
+      return $data;
+    } else {
+      return $error; 
+    }
+  }
+
 ?>
