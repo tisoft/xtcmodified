e1c170ae57561bf81036ae6a8a265571bef7a735
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-11-01 12:11:35 +0000

    check for defined('PHP4_3_10')
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1454 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_db_perform.inc.php
+++ inc/xtc_db_perform.inc.php
@@ -1,21 +1,21 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id$   
+   $Id$
 
    xtcModified - community made shopping
    http://www.xtc-modified.org
 
    Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
-   based on: 
+   based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(database.php,v 1.19 2003/03/22); www.oscommerce.com 
-   (c) 2003	 nextcommerce (xtc_db_perform.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2002-2003 osCommerce(database.php,v 1.19 2003/03/22); www.oscommerce.com
+   (c) 2003	nextcommerce (xtc_db_perform.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
    (c) 2006 XT-Commerce (xtc_db_perform.inc.php 899 2005-04-29)
 
-   Released under the GNU General Public License 
+   Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
-   
+
   function xtc_db_perform($table, $data, $action = 'insert', $parameters = '', $link = 'db_link') {
     reset($data);
 
@@ -27,7 +27,7 @@
       $query = substr($query, 0, -2) . ') values (';
       reset($data);
       while (list(, $value) = each($data)) {
-      	 $value = (is_Float($value) & PHP4_3_10) ? sprintf("%.F",$value) : (string)($value);
+      	 $value = (is_Float($value) & defined('PHP4_3_10')) ? sprintf("%.F",$value) : (string)($value);
         switch ($value) {
           case 'now()':
             $query .= 'now(), ';
@@ -44,7 +44,7 @@
     } elseif ($action == 'update') {
       $query = 'update ' . $table . ' set ';
       while (list($columns, $value) = each($data)) {
-         $value = (is_Float($value) & PHP4_3_10) ? sprintf("%.F",$value) : (string)($value);
+         $value = (is_Float($value) & defined('PHP4_3_10')) ? sprintf("%.F",$value) : (string)($value);
       	switch ($value) {
           case 'now()':
             $query .= $columns . ' = now(), ';
