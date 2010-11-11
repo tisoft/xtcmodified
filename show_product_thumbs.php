a279e3e0a18dde9d118513e0c093ff929febaa2e
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-11-11 16:23:14 +0000

    Party revoked r69 (replaced tables with divs, revert code changes to make it easier to compare code changes to other forks and the original xtcommerce versions)
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1473 25741c12-d480-4641-b65d-acd752be3b7f

--- show_product_thumbs.php
+++ show_product_thumbs.php
@@ -1,16 +1,16 @@
 <?php
-
 /* -----------------------------------------------------------------------------------------
-   $Id: show_product_thumbs.php 831 2005-03-13 10:16:09Z mz $   
+   $Id$   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(popup_image.php,v 1.12 2001/12/12); www.oscommerce.com 
+   (c) 2005 xtCommerce (show_product_thumbs.php 831 2005-03-13 10:16:09Z mz); www.xt-commerce.com
 
    Released under the GNU General Public License 
    -----------------------------------------------------------------------------------------
@@ -20,58 +20,44 @@
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
-require ('includes/application_top.php');
-require_once (DIR_FS_INC.'xtc_get_products_mo_images.inc.php');
+  require ('includes/application_top.php');
+  require_once (DIR_FS_INC.'xtc_get_products_mo_images.inc.php');
 ?>
+<body>
+<table align="center">
+    <tr>
 <?php
-//BOF - Dokuman - 2009-06-05 - replace table with div
-//<body bgcolor="#FFFFFF">
-//<table align="center">
-//<tr>
-echo '<body>';
-echo '<div align="center">';
-//EOF - Dokuman - 2009-06-05 - replace table with div
-?>
-<?php
-$mo_images = xtc_get_products_mo_images((int) $_GET['pID']);
-if ((int) $_GET['imgID'] == 0)
-	$actual = ' bgcolor="#FF0000"';
-else
-	unset ($actual);
-//BOF - Dokuman - 2009-06-05 - replace table with div
-//echo '<td align="left"'.$actual.'>';
-echo '<div style="display:inline">';
-//EOF - Dokuman - 2009-06-05 - replace table with div
-
-$products_query = xtc_db_query("select pd.products_name, p.products_image from ".TABLE_PRODUCTS." p left join ".TABLE_PRODUCTS_DESCRIPTION." pd on p.products_id = pd.products_id where p.products_status = '1' and p.products_id = '".(int) $_GET['pID']."' and pd.language_id = '".(int) $_SESSION['languages_id']."'");
-$products_values = xtc_db_fetch_array($products_query);
-//BOF - Dokuman - 2009-06-05 - replace table with div
-//echo '<a href="popup_image.php?pID='.(int) $_GET['pID'].'&imgID=0" target="_parent">'.xtc_image(DIR_WS_THUMBNAIL_IMAGES.$products_values['products_image'], $products_values['products_name']).'</a>';
-//echo '</td>';
-echo '<a href="popup_image.php?pID='.(int) $_GET['pID'].'&imgID=0" target="_parent">'.xtc_image(DIR_WS_THUMBNAIL_IMAGES.$products_values['products_image'], $products_values['products_name']).'</a>&nbsp;&nbsp;';
-//EOF - Dokuman - 2009-06-05 - replace table with div
-
-if ($mo_images != false) {
-	foreach ($mo_images as $mo_img) {
-		if ($mo_img['image_nr'] == (int) $_GET['imgID'])
-			$actual = ' bgcolor="#FF0000"';
-		else
-			unset ($actual);
-//BOF - Dokuman - 2009-06-05 - replace table with div
-		//echo '<td align=left'.$actual.'><a href="popup_image.php?pID='.(int) $_GET['pID'].'&imgID='.$mo_img['image_nr'].'" target="_parent">'.xtc_image(DIR_WS_THUMBNAIL_IMAGES.$mo_img['image_name'], $products_values['products_name']).'</a></td>';
-		echo '<a href="popup_image.php?pID='.(int) $_GET['pID'].'&imgID='.$mo_img['image_nr'].'" target="_parent">'.xtc_image(DIR_WS_THUMBNAIL_IMAGES.$mo_img['image_name'], $products_values['products_name']).'</a>&nbsp;&nbsp;';
-//EOF - Dokuman - 2009-06-05 - replace table with div
-
-	}
-}
-//BOF - Dokuman - 2009-06-05 - replace table with div
-/*
+  $mo_images = xtc_get_products_mo_images((int) $_GET['pID']);
+  if ((int) $_GET['imgID'] == 0){
+    $actual = ' style="background: #ff0000"';
+  } else {
+    $actual = '';
+  }
+  echo '<td align="left"'.$actual.'>';
+  
+  $products_query = xtc_db_query("select 
+                    pd.products_name,
+                    p.products_image
+                    from ".TABLE_PRODUCTS." p
+                    left join ".TABLE_PRODUCTS_DESCRIPTION." pd on p.products_id = pd.products_id
+                    where p.products_status = '1'
+                    and p.products_id = '".(int) $_GET['pID']."'
+                    and pd.language_id = '".(int) $_SESSION['languages_id']."'");
+  
+  $products_values = xtc_db_fetch_array($products_query);
+  echo '<a href="popup_image.php?pID='.(int) $_GET['pID'].'&imgID=0" target="_parent">'.xtc_image(DIR_WS_THUMBNAIL_IMAGES.$products_values['products_image'], $products_values['products_name']).'</a>';
+  echo '</td>';
+  if ($mo_images != false) {
+    foreach ($mo_images as $mo_img) {
+      if ($mo_img['image_nr'] == (int) $_GET['imgID']) {
+          $actual = ' style="background: #ff0000;"';
+      } else {
+          $actual = '';
+      }
+      echo '<td align=left'.$actual.'><a href="popup_image.php?pID='.(int) $_GET['pID'].'&imgID='.$mo_img['image_nr'].'" target="_parent">'.xtc_image(DIR_WS_THUMBNAIL_IMAGES.$mo_img['image_name'], $products_values['products_name']).'</a></td>';
+    }
+  }
 ?>
 </tr>
 </table>
-*/
-echo '  </div>';
-echo '</div>';
-//EOF - Dokuman - 2009-06-05 - replace table with div
-?>
 </body>
