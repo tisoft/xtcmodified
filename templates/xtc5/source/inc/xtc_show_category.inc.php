e18ee76619561da1c1e947f4227c439953cd790c
web28 <web28@25741c12-d480-4641-b65d-acd752be3b7f>
2010-11-12 14:13:47 +0000

    minor changes in xtc_show_category.inc.php
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1478 25741c12-d480-4641-b65d-acd752be3b7f

--- templates/xtc5/source/inc/xtc_show_category.inc.php
+++ templates/xtc5/source/inc/xtc_show_category.inc.php
@@ -1,7 +1,7 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: xtc_show_category.inc.php,v 1.5 2008/04/11 15:30:00 Hetfield $
-   
+   $Id$
+
    xtcModified - community made shopping
    http://www.xtc-modified.org
 
@@ -16,8 +16,7 @@
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(categories.php,v 1.23 2002/11/12); www.oscommerce.com
    (c) 2003	 nextcommerce (xtc_show_category.inc.php,v 1.4 2003/08/13); www.nextcommerce.org 
-   (c) 2003  XT-Commerce (xtc_show_category.inc.php 1262 2005-09-30); www.xt-commerce.com
-   (c) 2010  web28 (xtc_show_category.inc.php, v 2.0 2010/11/11); www.rpa-com.de
+   (c) 2010  web28 (xtc_show_category.inc.php, v 2.1 2010/11/12); www.rpa-com.de
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/   
@@ -25,23 +24,23 @@
 function xtc_show_category($counter, $oldlevel=1) {
 	global $foo, $categories_string, $id, $cPath;	
 	
-	//BOF +++ UL LI Verschachtelung  mit Quelltext Tab Einz¸gen +++
-		$newlevel = $foo[$counter]['level']+1;
-		$ul = $tab_ul = $tab_li = '';	
-		for ($i = 1; $i <= $newlevel; $i++) {
-			$tab_ul .= "\t";
-		}
-		$tab_li = $tab_ul;
+	$level = $foo[$counter]['level']+1;
+	
+	//BOF +++ UL LI Verschachtelung  mit Quelltext Tab Einz¸gen +++		
+		$ul = $tab = '';	
+		for ($i = 1; $i <= $level; $i++) {
+			$tab .= "\t";
+		}		
 		
-		if ($newlevel > $oldlevel) { //neue Unterebene
-			$ul = "\n" . $tab_ul. '<ul>'. "\n";
+		if ($level > $oldlevel) { //neue Unterebene
+			$ul = "\n" . $tab. '<ul>'. "\n";
 			$categories_string = rtrim($categories_string, "\n"); //Zeilenumbruch entfernen
 			$categories_string = substr($categories_string, 0, strlen($categories_string) -5);  //letztes  </li>  entfernen	
 		} 
-		elseif ($newlevel < $oldlevel) { //zur¸ck zur hˆheren Ebene
-			$ul = close_ul_tags($newlevel,$oldlevel);			
+		elseif ($level < $oldlevel) { //zur¸ck zur hˆheren Ebene
+			$ul = close_ul_tags($level,$oldlevel);			
 		}
-		$oldlevel = $newlevel;
+		
 	//EOF +++ UL LI Verschachtelung  mit Quelltext Tab Einz¸gen +++
 	
 	//BOF +++ Kategorien markieren +++
@@ -50,12 +49,12 @@ function xtc_show_category($counter, $oldlevel=1) {
 		//Elternkategorie markieren
 		$cat_active_parent = '';
 		$in_path = in_array($counter, $category_path); //Testen, ob aktuelle Kategorie ID im Kategoriepfad enthalten ist
-		if ($in_path) $cat_active_parent = " activeparent".($foo[$counter]['level']+1); 
+		if ($in_path) $cat_active_parent = " activeparent".$level; 
 		
 		//Aktive Kategorie markieren
 		$cat_active = '';
 		$this_category = array_pop($category_path); //Letzter Eintrag im Array ist die aktuelle Kategorie
-		if ($this_category == $counter) $cat_active = " active".($foo[$counter]['level']+1);
+		if ($this_category == $counter) $cat_active = " active".$level;
 	//EOF +++ Kategorien markieren +++
 	
 	
@@ -65,8 +64,8 @@ function xtc_show_category($counter, $oldlevel=1) {
 		if (trim($categories_string == '')) $categories_string = "\n"; //Zeilenschaltung Codedarstellung	
 		
 		$categories_string .= $ul; //UL LI Versschachtelung
-		$categories_string .= $tab_li; //Tabulator Codedarstellung
-		$categories_string .= '<li class="level'.($foo[$counter]['level']+1).$cat_active.$cat_active_parent.'">';
+		$categories_string .= $tab; //Tabulator Codedarstellung
+		$categories_string .= '<li class="level'.$level.$cat_active.$cat_active_parent.'">';
 		$categories_string .= '<a href="'.xtc_href_link(FILENAME_DEFAULT, $cPath_new).'" title="'. $foo[$counter]['name'] . '">';
 		$categories_string .= $foo[$counter]['name'];
 		
@@ -84,18 +83,18 @@ function xtc_show_category($counter, $oldlevel=1) {
 
 	//N‰chste Kategorie
 	if ($foo[$counter]['next_id']) {
-		xtc_show_category($foo[$counter]['next_id'], $oldlevel);
+		xtc_show_category($foo[$counter]['next_id'], $level);
 	} else {	
-		if ($oldlevel > 1) $categories_string .= close_ul_tags(1,$oldlevel);
+		if ($level > 1) $categories_string .= close_ul_tags(1,$level);
 		return;
 	}
 }
 
 //Alle offenen UL LI Tags schlieﬂen
-function close_ul_tags($newlevel, $oldlevel) {
+function close_ul_tags($level, $oldlevel) {
 	$count = 1;
 	$ul = '';
-	while($count <= $oldlevel - $newlevel) { //f¸r jede Ebene die UL LI Tags schlieﬂen
+	while($count <= $oldlevel - $level) { //f¸r jede Ebene die UL LI Tags schlieﬂen
 		$tab_end = '';
 		for ($i = 1; $i <= $oldlevel - $count; $i++) {
 			$tab_end .= "\t";
