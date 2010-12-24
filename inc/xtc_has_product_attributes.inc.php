0235cfa8dfab7ddb80f1fcc432d553b55d3dd4ce
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2010-12-24 14:10:13 +0000

    Fixed SQL-Injection vulnerability
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1589 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_has_product_attributes.inc.php
+++ inc/xtc_has_product_attributes.inc.php
@@ -1,24 +1,26 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: xtc_has_product_attributes.inc.php 1009 2005-07-11 16:19:29Z mz $   
+   $Id$
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
-   based on: 
+   based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(general.php,v 1.225 2003/05/29); www.oscommerce.com 
-   (c) 2003	 nextcommerce (xtc_has_product_attributes.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2002-2003 osCommerce(general.php,v 1.225 2003/05/29); www.oscommerce.com
+   (c) 2003 nextcommerce (xtc_has_product_attributes.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2006 XT-Commerce (xtc_has_product_attributes.inc.php 1009 2005-07-11)
 
-   Released under the GNU General Public License 
+   Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
 // Check if product has attributes
   function xtc_has_product_attributes($products_id) {
-    $attributes_query = "select count(*) as count from " . TABLE_PRODUCTS_ATTRIBUTES . " where products_id = '" . $products_id . "'";
-    $attributes_query  = xtDBquery($attributes_query);
+  
+    $attributes_query = "select count(*) as count from " . TABLE_PRODUCTS_ATTRIBUTES . " where products_id = '" . (int)$products_id . "'";
+    $attributes_query = xtDBquery($attributes_query);
     $attributes = xtc_db_fetch_array($attributes_query,true);
 
     if ($attributes['count'] > 0) {
