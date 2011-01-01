7abf4d0c8622c6d76c0b4709f4d0320c00c04d62
franky-n-xtcm <franky-n-xtcm@25741c12-d480-4641-b65d-acd752be3b7f>
2011-01-01 15:09:51 +0000

    some code formatting
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1599 25741c12-d480-4641-b65d-acd752be3b7f

--- templates/xtc5/javascript/general.js.php
+++ templates/xtc5/javascript/general.js.php
@@ -22,27 +22,25 @@
 <script src="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/javascript/jquery.js" type="text/javascript"></script>
 <script src="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/javascript/thickbox.js" type="text/javascript"></script>
 <?php
-// BOF - web28 - 2010-07-26 - TABS/ACCORDION in product_info
-if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO )) {
-?>
-<script src="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/javascript/jquery-ui.js" type="text/javascript"></script>
-<script type="text/javascript">
-<!-- 
-    //Laden einer CSS Datei mit jquery
-    $.get("<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>"+"/css/javascript.css",
-    function(css) {
-      $("head").append("<style type='text/css'>"+css+"<\/style>");
-    });
-
-  $(function() {
-    $("#tabbed_product_info").tabs();
-    $("#accordion_product_info").accordion({ autoHeight: false });
-  });
-//-->
-</script>
-<?php
-}
-// EOF - web28 - 2010-07-26 - TABS/ACCORDION in product_info
-//EOF - GTB - 2010-08-03 - Security Fix - Base
-//eOF - web28 - 2010-09-03 - Security Fix - Base -> add DIR_WS_CATALOG to template path
+  // BOF - web28 - 2010-07-26 - TABS/ACCORDION in product_info
+  if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO )) {
+    ?>
+    <script src="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/javascript/jquery-ui.js" type="text/javascript"></script>
+    <script type="text/javascript">
+      <!--  //Laden einer CSS Datei mit jquery
+      $.get("<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>"+"/css/javascript.css",
+      function(css) {
+        $("head").append("<style type='text/css'>"+css+"<\/style>");
+      });
+      $(function() {
+        $("#tabbed_product_info").tabs();
+        $("#accordion_product_info").accordion({ autoHeight: false });
+      });
+      //-->
+    </script>
+    <?php
+  }
+  // EOF - web28 - 2010-07-26 - TABS/ACCORDION in product_info
+  //EOF - GTB - 2010-08-03 - Security Fix - Base
+  //eOF - web28 - 2010-09-03 - Security Fix - Base -> add DIR_WS_CATALOG to template path
 ?>
