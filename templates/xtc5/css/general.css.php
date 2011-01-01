f6c2b6bd72911291676df5ce014849ef3b31887b
franky-n-xtcm <franky-n-xtcm@25741c12-d480-4641-b65d-acd752be3b7f>
2011-01-01 14:47:55 +0000

    some code formatting...
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1598 25741c12-d480-4641-b65d-acd752be3b7f

--- templates/xtc5/css/general.css.php
+++ templates/xtc5/css/general.css.php
@@ -16,24 +16,25 @@
    // Put CSS-Definitions here, these CSS-files will be loaded at the TOP of every page
 ?>
 <?php 
-/*
-//BOF - GTB - 2010-08-03 - Security Fix - Base
-<link rel="stylesheet" href="<?php echo 'templates/'.CURRENT_TEMPLATE; ?>/stylesheet.css" type="text/css" />
-<link rel="stylesheet" href="<?php echo 'templates/'.CURRENT_TEMPLATE; ?>/css/thickbox.css" type="text/css" media="screen" />
-*/ ?>
+  /*
+  //BOF - GTB - 2010-08-03 - Security Fix - Base
+  <link rel="stylesheet" href="<?php echo 'templates/'.CURRENT_TEMPLATE; ?>/stylesheet.css" type="text/css" />
+  <link rel="stylesheet" href="<?php echo 'templates/'.CURRENT_TEMPLATE; ?>/css/thickbox.css" type="text/css" media="screen" />
+  */ 
+?>
 <link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/stylesheet.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/thickbox.css" type="text/css" media="screen" />
 <?php 
-//EOF - GTB - 2010-08-03 - Security Fix - Base
+  //EOF - GTB - 2010-08-03 - Security Fix - Base
 ?>
 <?php 
-// BOF - web28 - 2010-07-09 - TABS/ACCORDION in product_info
-if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO )) {
-//BOF - GTB - 2010-08-03 - Security Fix - Base ?>
-<link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/jquery-ui.css" type="text/css" media="screen" />
-<?php
-/* <link rel="stylesheet" href="<?php echo 'templates/'.CURRENT_TEMPLATE; ?>/css/jquery-ui.css" type="text/css" media="screen" />
-//EOF - GTB - 2010-08-03 - Security Fix - Base */
-}
-// EOF - web28 - 2010-07-09 - TABS/ACCORDION in product_info
+  // BOF - web28 - 2010-07-09 - TABS/ACCORDION in product_info
+  if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO )) {
+    //BOF - GTB - 2010-08-03 - Security Fix - Base
+    ?>
+    <link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/jquery-ui.css" type="text/css" media="screen" />
+    <?php
+    /* <link rel="stylesheet" href="<?php echo 'templates/'.CURRENT_TEMPLATE; ?>/css/jquery-ui.css" type="text/css" media="screen" /> //EOF - GTB - 2010-08-03 - Security Fix - Base */
+  }
+  // EOF - web28 - 2010-07-09 - TABS/ACCORDION in product_info
 ?>
