4627a8181dbf10627bccf3b45488abca0ec3b067
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2011-01-02 19:36:20 +0000

    removed unnecessary blank in title-tag
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1606 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_image_submit.inc.php
+++ inc/xtc_image_submit.inc.php
@@ -7,23 +7,23 @@
 
    Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
-   based on: 
+   based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(html_output.php,v 1.52 2003/03/19); www.oscommerce.com 
-   (c) 2003	 nextcommerce (xtc_image_submit.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2002-2003 osCommerce(html_output.php,v 1.52 2003/03/19); www.oscommerce.com
+   (c) 2003	nextcommerce (xtc_image_submit.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
    (c) 2006 XT-Commerce
 
-   Released under the GNU General Public License 
+   Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
-   
+
 // The HTML form submit button wrapper function
 // Outputs a button in the selected language
   function xtc_image_submit($image, $alt = '', $parameters = '') {
-	//BOF - GTB - 2010-08-03 - Security Fix - Base
+    //BOF - GTB - 2010-08-03 - Security Fix - Base
     $image_submit = '<input type="image" src="' . xtc_parse_input_field_data(DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.'/buttons/' . $_SESSION['language'] . '/'. $image, array('"' => '&quot;')) . '" alt="' . xtc_parse_input_field_data($alt, array('"' => '&quot;')) . '"';
     //$image_submit = '<input type="image" src="' . xtc_parse_input_field_data('templates/'.CURRENT_TEMPLATE.'/buttons/' . $_SESSION['language'] . '/'. $image, array('"' => '&quot;')) . '" alt="' . xtc_parse_input_field_data($alt, array('"' => '&quot;')) . '"';
-	//EOF - GTB - 2010-08-03 - Security Fix - Base
-    if (xtc_not_null($alt)) $image_submit .= ' title=" ' . xtc_parse_input_field_data($alt, array('"' => '&quot;')) . ' "';
+    //EOF - GTB - 2010-08-03 - Security Fix - Base
+    if (xtc_not_null($alt)) $image_submit .= ' title="' . xtc_parse_input_field_data($alt, array('"' => '&quot;')) . '"';
 
     if (xtc_not_null($parameters)) $image_submit .= ' ' . $parameters;
 
@@ -31,4 +31,4 @@
 
     return $image_submit;
   }
- ?>
+?>
