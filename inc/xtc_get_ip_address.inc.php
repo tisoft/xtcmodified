02745de2704e820e1dae02463aeb5eecbcfce74f
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2011-01-06 16:11:48 +0000

    Improve IP address detection and add support for multiple proxies
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1614 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_get_ip_address.inc.php
+++ inc/xtc_get_ip_address.inc.php
@@ -1,21 +1,47 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: xtc_get_ip_address.inc.php 899 2005-04-29 02:40:57Z hhgag $   
+   $Id$
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
-   based on: 
+   based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(general.php,v 1.225 2003/05/29); www.oscommerce.com 
-   (c) 2003	 nextcommerce (xtc_get_ip_address.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2002-2003 osCommerce(general.php,v 1.225 2003/05/29); www.oscommerce.com
+   (c) 2003 nextcommerce (xtc_get_ip_address.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2006 xt-commerce (xtc_get_ip_address.inc.php 899 2005-04-29); www.xt-commerce.com
 
-   Released under the GNU General Public License 
+   Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
-   
+
+  //BOF - 2010-01-06 - Improve IP Address Detection
+  function xtc_validate_ip_address($ip_address) {
+    if (function_exists('filter_var') && defined('FILTER_VALIDATE_IP')) {
+      return filter_var($ip_address, FILTER_VALIDATE_IP, array('flags' => FILTER_FLAG_IPV4));
+    }
+
+    if (preg_match('/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/', $ip_address)) {
+      $parts = explode('.', $ip_address);
+
+      foreach ($parts as $ip_parts) {
+        if ( (intval($ip_parts) > 255) || (intval($ip_parts) < 0) ) {
+          return false; // number is not within 0-255
+        }
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+  //EOF - 2010-01-06 - Improve IP Address Detection
+
+
   function xtc_get_ip_address() {
+    //BOF - 2010-01-06 - Improve IP Address Detection
+    /*
     if (isset($_SERVER)) {
       if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
@@ -35,5 +61,43 @@
     }
 
     return $ip;
+    */
+
+    $ip_address = null;
+    $ip_addresses = array();
+
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      foreach ( array_reverse(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])) as $x_ip ) {
+        $x_ip = trim($x_ip);
+
+        if (xtc_validate_ip_address($x_ip)) {
+          $ip_addresses[] = $x_ip;
+        }
+      }
+    }
+
+    if (isset($_SERVER['HTTP_CLIENT_IP']) && !empty($_SERVER['HTTP_CLIENT_IP'])) {
+      $ip_addresses[] = $_SERVER['HTTP_CLIENT_IP'];
+    }
+
+    if (isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && !empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
+      $ip_addresses[] = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+    }
+
+    if (isset($_SERVER['HTTP_PROXY_USER']) && !empty($_SERVER['HTTP_PROXY_USER'])) {
+      $ip_addresses[] = $_SERVER['HTTP_PROXY_USER'];
+    }
+
+    $ip_addresses[] = $_SERVER['REMOTE_ADDR'];
+
+    foreach ( $ip_addresses as $ip ) {
+      if (!empty($ip) && xtc_validate_ip_address($ip)) {
+        $ip_address = $ip;
+        break;
+      }
+    }
+
+    return $ip_address;
+    //EOF - 2010-01-06 - Improve IP Address Detection
   }
  ?>
