d068729f71b5f4768913d35955b1172e7619ab6f
web28 <web28@25741c12-d480-4641-b65d-acd752be3b7f>
2011-01-08 16:30:16 +0000

    delete unnecessary comments in case 'add_product', fix add_cart to original code (wrong $cart_quantity)
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1623 25741c12-d480-4641-b65d-acd752be3b7f

--- includes/cart_actions.php
+++ includes/cart_actions.php
@@ -116,8 +116,9 @@ if (xtc_not_null($action)) {
 
 				$cart_quantity = xtc_remove_non_numeric($_POST['products_qty']);
 
-				if ($cart_quantity > MAX_PRODUCTS_QTY)
+				if ($cart_quantity > MAX_PRODUCTS_QTY) {
 					$cart_quantity = MAX_PRODUCTS_QTY;
+        }
 
 				if (isset($econda) && is_object($econda)) {
 					$econda->_emptyCart();
@@ -125,15 +126,7 @@ if (xtc_not_null($action)) {
 					$econda->_addProduct($_POST['products_id'], $cart_quantity, $old_quantity);
 				}
 
-        //BOF - Dokuman - 2010-02-25 - fix 'Fatal error: Call to a member function add_cart()'
-				//$_SESSION['cart']->add_cart((int) $_POST['products_id'], $_SESSION['cart']->get_quantity(xtc_get_uprid($_POST['products_id'], $_POST['id'])) + $cart_quantity, $_POST['id']);
-        //BOF - Dokuman - 2010-11-16 - fix 'Fatal error: Call to a member function remove(), add_cart() for all actions
-        //if (!is_object($_SESSION['cart'])) {
-        //  $_SESSION['cart'] = new shoppingCart();
-        //}
-        //BOF - Dokuman - 2010-11-16 - fix 'Fatal error: Call to a member function remove(), add_cart() for all actions
-        $_SESSION['cart']->add_cart((int) $_POST['products_id'], $_SESSION['cart']->get_quantity(xtc_get_uprid($_POST['products_id'], $_POST['id'])) + xtc_remove_non_numeric($_POST['products_qty']), $_POST['id']);
-        //EOF - Dokuman - 2010-02-25 - fix 'Fatal error: Call to a member function add_cart()'
+        $_SESSION['cart']->add_cart((int) $_POST['products_id'], $_SESSION['cart']->get_quantity(xtc_get_uprid($_POST['products_id'], $_POST['id'])) + $cart_quantity, $_POST['id']);        
 
 			}
 			xtc_redirect(xtc_href_link($goto, 'products_id=' . (int) $_POST['products_id'] . '&' . xtc_get_all_get_params($parameters)));
