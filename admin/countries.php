5d6672d8eb119f4048bed4d7b88154a89a9dfe44
franky-n-xtcm <franky-n-xtcm@25741c12-d480-4641-b65d-acd752be3b7f>
2011-01-17 18:45:41 +0000

    fixing buttons top of countries
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1638 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/countries.php
+++ admin/countries.php
@@ -165,11 +165,11 @@
                               <!-- BOF - Tomcraft - 2009-06-10 - added some missing alternative text on admin icons -->
                               <!-- <td class="dataTableContent" align="right"> -->
                               <?php 
-                                if ( (isset($cInfo) && (is_object($cInfo)) && ($countries['countries_id'] == $cInfo->countries_id) ) ) { 
-                                  echo xtc_image(DIR_WS_IMAGES . 'icon_arrow_right.gif', ''); 
-                                } else { 
-                                  echo '<a href="' . xtc_href_link(FILENAME_COUNTRIES, 'page=' . $_GET['page'] . '&cID=' . $countries['countries_id']) . '">' . xtc_image(DIR_WS_IMAGES . 'icon_info.gif', IMAGE_ICON_INFO).'</a>';
-                                } 
+                              //  if ( (isset($cInfo) && (is_object($cInfo)) && ($countries['countries_id'] == $cInfo->countries_id) ) ) { 
+                              //    echo xtc_image(DIR_WS_IMAGES . 'icon_arrow_right.gif', ''); 
+                              //  } else { 
+                              //    echo '<a href="' . xtc_href_link(FILENAME_COUNTRIES, 'page=' . $_GET['page'] . '&cID=' . $countries['countries_id']) . '">' . xtc_image(DIR_WS_IMAGES . 'icon_info.gif', IMAGE_ICON_INFO).'</a>';
+                              //  } 
                               ?>
                               <!-- &nbsp;</td> -->
                               <td class="dataTableContent" align="right"><?php if ( (isset($cInfo) && (is_object($cInfo)) && ($countries['countries_id'] == $cInfo->countries_id) ) ) { echo xtc_image(DIR_WS_IMAGES . 'icon_arrow_right.gif', ICON_ARROW_RIGHT); } else { echo '<a href="' . xtc_href_link(FILENAME_COUNTRIES, 'page=' . $_GET['page'] . '&cID=' . $countries['countries_id']) . '">' . xtc_image(DIR_WS_IMAGES . 'icon_info.gif', IMAGE_ICON_INFO) . '</a>'; } ?>&nbsp;</td>
