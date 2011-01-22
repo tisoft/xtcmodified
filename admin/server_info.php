e55744a089f417e918252ac8b0ef83fbacf08803
franky-n-xtcm <franky-n-xtcm@25741c12-d480-4641-b65d-acd752be3b7f>
2011-01-22 16:59:33 +0000

    some code formatted
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1684 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/server_info.php
+++ admin/server_info.php
@@ -1,5 +1,5 @@
 <?php
-/* --------------------------------------------------------------
+  /* --------------------------------------------------------------
    $Id$
 
    xtcModified - community made shopping
@@ -16,158 +16,160 @@
    Released under the GNU General Public License
    --------------------------------------------------------------*/
 
-require('includes/application_top.php');
-
-if (isset($_REQUEST['phpInfo'])) {
-  phpinfo();
-  exit;
-}
-
-$system = xtc_get_system_information();
+  require('includes/application_top.php');
+  if (isset($_REQUEST['phpInfo'])) {
+    phpinfo();
+    exit;
+  }
+  $system = xtc_get_system_information();
 ?>
 <!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html <?php echo HTML_PARAMS; ?>>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>">
-<title><?php echo TITLE; ?></title>
-<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
-</head>
-<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
-<!-- header //-->
-<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
-<!-- header_eof //-->
-
-<!-- body //-->
-<table border="0" width="100%" cellspacing="2" cellpadding="2">
-  <tr>
-    <td class="columnLeft2" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
-<!-- left_navigation //-->
-<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
-<!-- left_navigation_eof //-->
-    </table></td>
-<!-- body_text //-->
-    <td class="boxCenter" width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>" />
+    <title><?php echo TITLE; ?></title>
+    <link rel="stylesheet" type="text/css" href="includes/stylesheet.css" />
+  </head>
+  <body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
+    <!-- header //-->
+    <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+    <!-- header_eof //-->
+    <!-- body //-->
+    <table border="0" width="100%" cellspacing="2" cellpadding="2">
       <tr>
-        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
-          <tr>
-            <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
-            <td class="pageHeading" align="right"><?php echo xtc_draw_separator('pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
-          </tr>
-        </table></td>
-      </tr>
-      <tr>
-        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
-          <tr>
-            <td align="left"><table border="0" cellspacing="0" cellpadding="3">
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_SERVER_HOST; ?></strong></td>
-                <td class="smallText"><?php echo $system['host'] . ' (' . $system['ip'] . ')'; ?></td>
-                <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_HOST; ?></strong></td>
-                <td class="smallText"><?php echo $system['db_server'] . ' (' . $system['db_ip'] . ')'; ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_SERVER_OS; ?></strong></td>
-                <td class="smallText"><?php echo $system['system'] . ' ' . $system['kernel']; ?></td>
-                <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE; ?></strong></td>
-                <td class="smallText"><?php echo $system['db_version']; ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_SERVER_DATE; ?></strong></td>
-                <td class="smallText"><?php echo $system['date']; ?></td>
-                <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_DATE; ?></strong></td>
-                <td class="smallText"><?php echo $system['db_date']; ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_SERVER_UP_TIME; ?></strong></td>
-                <td colspan="3" class="smallText"><?php echo $system['uptime']; ?></td>
-              </tr>
-              <tr>
-                <td colspan="4"><?php echo xtc_draw_separator('pixel_trans.gif', '1', '5'); ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_HTTP_SERVER; ?></strong></td>
-                <td colspan="3" class="smallText"><?php echo $system['http_server']; ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_PHP_VERSION; ?></strong></td>
-                <td colspan="3" class="smallText"><?php echo $system['php'] . ' (' . TITLE_ZEND_VERSION . ' ' . $system['zend'] . ')'; ?></td>
-              </tr>
-            </table></td>
-          </tr>
-        </table></td>
-      </tr>
-      <tr>
-        <td><?php echo xtc_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
-      </tr>
-      <tr>
-        <!--td>
-           <iframe src="?phpInfo" width="100%" height="700" style="border: solid 1px #a3a3a3;">
-           <p>Der verwendete Browser kann leider nicht mit inline Frames (iframe)
-             umgehen:
-             <a href="?phpInfo" target="_blank">Hier geht es zur phpinfo()
-             Seite vom System</a>
-           </p>
-         </iframe>
-        </td-->
-	<td align="center">
-		   <div style="width:980px; border: solid 1px #a3a3a3; padding:10px; overflow:auto; height:600px;">
-           <?php
-			  if (function_exists('ob_start')) {
-			    ob_start();
-			    phpinfo();
-			    $phpinfo = ob_get_contents();
-			    ob_end_clean();
-			    $phpinfo = str_replace('border: 1px', '', $phpinfo);
-
-			    //BOF - DokuMan - 2010-09-16 - replace ereg by preg_match
-			    /*
-			    ereg("(<style type=\"text/css\">{1})(.*)(</style>{1})", $phpinfo, $regs);
-          //BOF css border and link  correction
-          $regs[2] = str_replace('.e {', '.e {border: 1px solid #000000; ', $regs[2]);
-          $regs[2] = str_replace('.v {', '.v {border: 1px solid #000000; ', $regs[2]);
-          $regs[2] = str_replace('.h {', '.h {border: 1px solid #000000; ', $regs[2]);
-          $regs[2] = str_replace('.h {', '.h {border: 1px solid #000000; ', $regs[2]);
-          $regs[2] = str_replace('a:link', 'a.phpinfo:link', $regs[2]);
-          $regs[2] = str_replace('a:hover', 'a.phpinfo:hover', $regs[2]);
-          //EOF css border and link correction
-			    echo '<style type="text/css">' . $regs[2] . '</style>';
-			    ereg("(<body>{1})(.*)(</body>{1})", $phpinfo, $regs);
-			    echo $regs[2];
-			    */
-
-			    preg_match("/<style type=\"text\/css\">(.*)<\/style>/is", $phpinfo, $regs);
-          //BOF css border and link correction
-          $regs[1] = str_replace('.e {', '.e {border: 1px solid #000000; ', $regs[1]);
-          $regs[1] = str_replace('.v {', '.v {border: 1px solid #000000; ', $regs[1]);
-          $regs[1] = str_replace('.h {', '.h {border: 1px solid #000000; ', $regs[1]);
-          $regs[1] = str_replace('.h {', '.h {border: 1px solid #000000; ', $regs[1]);
-          $regs[1] = str_replace('img {float: right; border: 0px;}', '', $regs[1]);
-          $regs[1] = str_replace('a:link', 'a.phpinfo:link', $regs[1]);
-          $regs[1] = str_replace('a:hover', 'a.phpinfo:hover', $regs[1]);
-          //EOF css border and link correction
-
-          echo '<style type="text/css">' . $regs[1] . '</style>';
-          preg_match("/<body>(.*)<\/body>/is", $phpinfo, $regs);
-          echo $regs[1];
-			    //EOF - DokuMan - 2010-09-16 - replace ereg by preg_match
-
-			  } else {
-			    phpinfo();
-			  }
-		   ?>
-		   </div>
+        <td class="columnLeft2" width="<?php echo BOX_WIDTH; ?>" valign="top">
+          <table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
+            <!-- left_navigation //-->
+            <?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+            <!-- left_navigation_eof //-->
+          </table>
         </td>
+        <!-- body_text //-->
+        <td class="boxCenter" width="100%" valign="top">
+          <table border="0" width="100%" cellspacing="0" cellpadding="2">
+            <tr>
+              <td>
+                <table border="0" width="100%" cellspacing="0" cellpadding="0">
+                  <tr>
+                    <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
+                    <td class="pageHeading" align="right"><?php echo xtc_draw_separator('pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <table border="0" width="100%" cellspacing="0" cellpadding="2">
+                  <tr>
+                    <td align="left">
+                      <table border="0" cellspacing="0" cellpadding="3">
+                        <tr>
+                          <td class="smallText"><strong><?php echo TITLE_SERVER_HOST; ?></strong></td>
+                          <td class="smallText"><?php echo $system['host'] . ' (' . $system['ip'] . ')'; ?></td>
+                          <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_HOST; ?></strong></td>
+                          <td class="smallText"><?php echo $system['db_server'] . ' (' . $system['db_ip'] . ')'; ?></td>
+                        </tr>
+                        <tr>
+                          <td class="smallText"><strong><?php echo TITLE_SERVER_OS; ?></strong></td>
+                          <td class="smallText"><?php echo $system['system'] . ' ' . $system['kernel']; ?></td>
+                          <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE; ?></strong></td>
+                          <td class="smallText"><?php echo $system['db_version']; ?></td>
+                        </tr>
+                        <tr>
+                          <td class="smallText"><strong><?php echo TITLE_SERVER_DATE; ?></strong></td>
+                          <td class="smallText"><?php echo $system['date']; ?></td>
+                          <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_DATE; ?></strong></td>
+                          <td class="smallText"><?php echo $system['db_date']; ?></td>
+                        </tr>
+                        <tr>
+                          <td class="smallText"><strong><?php echo TITLE_SERVER_UP_TIME; ?></strong></td>
+                          <td colspan="3" class="smallText"><?php echo $system['uptime']; ?></td>
+                        </tr>
+                        <tr>
+                          <td colspan="4"><?php echo xtc_draw_separator('pixel_trans.gif', '1', '5'); ?></td>
+                        </tr>
+                        <tr>
+                          <td class="smallText"><strong><?php echo TITLE_HTTP_SERVER; ?></strong></td>
+                          <td colspan="3" class="smallText"><?php echo $system['http_server']; ?></td>
+                        </tr>
+                        <tr>
+                          <td class="smallText"><strong><?php echo TITLE_PHP_VERSION; ?></strong></td>
+                          <td colspan="3" class="smallText"><?php echo $system['php'] . ' (' . TITLE_ZEND_VERSION . ' ' . $system['zend'] . ')'; ?></td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td><?php echo xtc_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
+            </tr>
+            <tr>
+              <!--td>
+              <iframe src="?phpInfo" width="100%" height="700" style="border: solid 1px #a3a3a3;">
+              <p>Der verwendete Browser kann leider nicht mit inline Frames (iframe)
+              umgehen:
+              <a href="?phpInfo" target="_blank">Hier geht es zur phpinfo()
+              Seite vom System</a>
+              </p>
+              </iframe>
+              </td-->
+              <td align="center">
+                <div style="width:980px; border: solid 1px #a3a3a3; padding:10px; overflow:auto; height:600px;">
+                  <?php
+                  if (function_exists('ob_start')) {
+                    ob_start();
+                    phpinfo();
+                    $phpinfo = ob_get_contents();
+                    ob_end_clean();
+                    $phpinfo = str_replace('border: 1px', '', $phpinfo);
+                    //BOF - DokuMan - 2010-09-16 - replace ereg by preg_match
+                    /*
+                    ereg("(<style type=\"text/css\">{1})(.*)(</style>{1})", $phpinfo, $regs);
+                    //BOF css border and link  correction
+                    $regs[2] = str_replace('.e {', '.e {border: 1px solid #000000; ', $regs[2]);
+                    $regs[2] = str_replace('.v {', '.v {border: 1px solid #000000; ', $regs[2]);
+                    $regs[2] = str_replace('.h {', '.h {border: 1px solid #000000; ', $regs[2]);
+                    $regs[2] = str_replace('.h {', '.h {border: 1px solid #000000; ', $regs[2]);
+                    $regs[2] = str_replace('a:link', 'a.phpinfo:link', $regs[2]);
+                    $regs[2] = str_replace('a:hover', 'a.phpinfo:hover', $regs[2]);
+                    //EOF css border and link correction
+                    echo '<style type="text/css">' . $regs[2] . '</style>';
+                    ereg("(<body>{1})(.*)(</body>{1})", $phpinfo, $regs);
+                    echo $regs[2];
+                    */
+                    preg_match("/<style type=\"text\/css\">(.*)<\/style>/is", $phpinfo, $regs);
+                    //BOF css border and link correction
+                    $regs[1] = str_replace('.e {', '.e {border: 1px solid #000000; ', $regs[1]);
+                    $regs[1] = str_replace('.v {', '.v {border: 1px solid #000000; ', $regs[1]);
+                    $regs[1] = str_replace('.h {', '.h {border: 1px solid #000000; ', $regs[1]);
+                    $regs[1] = str_replace('.h {', '.h {border: 1px solid #000000; ', $regs[1]);
+                    $regs[1] = str_replace('img {float: right; border: 0px;}', '', $regs[1]);
+                    $regs[1] = str_replace('a:link', 'a.phpinfo:link', $regs[1]);
+                    $regs[1] = str_replace('a:hover', 'a.phpinfo:hover', $regs[1]);
+                    //EOF css border and link correction
+                    echo '<style type="text/css">' . $regs[1] . '</style>';
+                    preg_match("/<body>(.*)<\/body>/is", $phpinfo, $regs);
+                    echo $regs[1];
+                    //EOF - DokuMan - 2010-09-16 - replace ereg by preg_match
+                  } else {
+                    phpinfo();
+                  }
+                  ?>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </td>
+        <!-- body_text_eof //-->
       </tr>
-    </table></td>
-<!-- body_text_eof //-->
-  </tr>
-</table>
-<!-- body_eof //-->
-
-<!-- footer //-->
-<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
-<!-- footer_eof //-->
-<br />
-</body>
+    </table>
+    <!-- body_eof //-->
+    <!-- footer //-->
+    <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+    <!-- footer_eof //-->
+    <br />
+  </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
