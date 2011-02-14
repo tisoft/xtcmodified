97e29d583acbfc8c2b73608f6f5111ee44f5de86
franky-n-xtcm <franky-n-xtcm@25741c12-d480-4641-b65d-acd752be3b7f>
2011-02-14 11:45:32 +0000

    some code formatted
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1800 25741c12-d480-4641-b65d-acd752be3b7f

--- admin/includes/liststyle_menu/topmenu.js
+++ admin/includes/liststyle_menu/topmenu.js
@@ -4,28 +4,28 @@
 var menuids=["treemenu1"] //Enter id(s) of SuckerTree UL menus, separated by commas
 
 function buildsubmenus_horizontal(){
-for (var i=0; i<menuids.length; i++){
-  var ultags=document.getElementById(menuids[i]).getElementsByTagName("ul")
+  for (var i=0; i<menuids.length; i++){
+    var ultags=document.getElementById(menuids[i]).getElementsByTagName("ul")
     for (var t=0; t<ultags.length; t++){
-    if (ultags[t].parentNode.parentNode.id==menuids[i]){ //if this is a first level submenu
-      ultags[t].style.top=(ultags[t].parentNode.offsetHeight) +"px" //dynamically position first level submenus to be height of main menu item
-      //ultags[t].parentNode.getElementsByTagName("a")[0].className="mainfoldericon"
-    }
-    else{ //else if this is a sub level menu (ul)
-      ultags[t].style.left=ultags[t-1].getElementsByTagName("a")[0].offsetWidth+"px" //position menu to the right of menu item that activated it
+      if (ultags[t].parentNode.parentNode.id==menuids[i]){ //if this is a first level submenu
+        ultags[t].style.top=(ultags[t].parentNode.offsetHeight) +"px" //dynamically position first level submenus to be height of main menu item
+        //ultags[t].parentNode.getElementsByTagName("a")[0].className="mainfoldericon"
+      } else { //else if this is a sub level menu (ul)
+        ultags[t].style.left=ultags[t-1].getElementsByTagName("a")[0].offsetWidth+"px" //position menu to the right of menu item that activated it
         //ultags[t].parentNode.getElementsByTagName("a")[0].className="subfoldericon"
-    }
-    ultags[t].parentNode.onmouseover=function(){
-    this.getElementsByTagName("ul")[0].style.visibility="visible"
-    }
-    ultags[t].parentNode.onmouseout=function(){
-    this.getElementsByTagName("ul")[0].style.visibility="hidden"
-    }
+      }
+      ultags[t].parentNode.onmouseover=function(){
+        this.getElementsByTagName("ul")[0].style.visibility="visible"
+      }
+      ultags[t].parentNode.onmouseout=function(){
+        this.getElementsByTagName("ul")[0].style.visibility="hidden"
+      }
     }
   }
 }
 
-if (window.addEventListener)
-window.addEventListener("load", buildsubmenus_horizontal, false)
-else if (window.attachEvent)
-window.attachEvent("onload", buildsubmenus_horizontal)
+if (window.addEventListener) {
+  window.addEventListener("load", buildsubmenus_horizontal, false)
+} else if (window.attachEvent) {
+  window.attachEvent("onload", buildsubmenus_horizontal)
+}
