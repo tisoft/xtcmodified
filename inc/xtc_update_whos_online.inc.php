d19c4fea2b303d422bb6323ed2c779d6def73625
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2011-03-18 19:34:56 +0000

    Fix display of correct HTTP_REFERER on refresh
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1837 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_update_whos_online.inc.php
+++ inc/xtc_update_whos_online.inc.php
@@ -1,31 +1,33 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: xtc_update_whos_online.inc.php 899 2005-04-29 02:40:57Z hhgag $   
+   $Id$
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
-   based on: 
+   based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(whos_online.php,v 1.8 2003/02/21); www.oscommerce.com 
-   (c) 2003	 nextcommerce (xtc_update_whos_online.inc.php,v 1.4 2003/08/13); www.nextcommerce.org
+   (c) 2002-2003 osCommerce(whos_online.php,v 1.8 2003/02/21); www.oscommerce.com
+   (c) 2003 nextcommerce (xtc_update_whos_online.inc.php,v 1.4 2003/08/13); www.nextcommerce.org
+   (c) 2006 XT-Commerce (xtc_update_whos_online.inc.php 899 2005-04-29)
 
-   Released under the GNU General Public License 
+   Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
   function xtc_update_whos_online() {
-    if (isset($_SESSION['customer_id'])) {
-      $wo_customer_id = $_SESSION['customer_id'];
 
-      $customer_query = xtc_db_query("select customers_firstname, customers_lastname from " . TABLE_CUSTOMERS . " where customers_id = '" . $_SESSION['customer_id'] . "'");
+    if (isset($_SESSION['customer_id'])) {
+      $wo_customer_id = (int)$_SESSION['customer_id'];
+
+      $customer_query = xtc_db_query("select customers_firstname, customers_lastname from " . TABLE_CUSTOMERS . " where customers_id = '" . $wo_customer_id . "'");
       $customer = xtc_db_fetch_array($customer_query);
 
       $wo_full_name = xtc_db_input($customer['customers_firstname'] . ' ' . $customer['customers_lastname']);
     } else {
       $wo_customer_id = '';
-      $wo_full_name = 'Guest';
+      $wo_full_name = TEXT_GUEST; //DokuMan - 2011-03-18 - use language dependent constant here
     }
 
     $wo_session_id = xtc_session_id();
@@ -34,7 +36,7 @@
     //$wo_last_page_url = addslashes(getenv('REQUEST_URI'));
     $wo_ip_address = xtc_db_input($_SERVER['REMOTE_ADDR']);
     $wo_last_page_url = xtc_db_input($_SERVER['REQUEST_URI']);
-    $wo_referer = xtc_db_input(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'Keiner/None');
+    $wo_referer = xtc_db_input(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '---');
     //EOF - Dokuman - 2009-10-28 - Who is online doesn't show any IP addresses and URLs (added http_referer)
 
     $current_time = time();
@@ -53,12 +55,32 @@
         full_name = '" . $wo_full_name . "',
         ip_address = '" . $wo_ip_address . "',
         time_last_click = '" . time() . "',
-        last_page_url = '" . $wo_last_page_url . "'
+        last_page_url = '" . $wo_last_page_url . "',
+        http_referer = '" . $wo_referer . "'
         where session_id = '" . $wo_session_id . "'");
     } else {
     //BOF - Dokuman - 2009-10-28 - Who is online: added http_referer
     //xtc_db_query("insert into " . TABLE_WHOS_ONLINE . " (customer_id, full_name, session_id, ip_address, time_entry, time_last_click, last_page_url) values ('" . $wo_customer_id . "', '" . $wo_full_name . "', '" . $wo_session_id . "', '" . $wo_ip_address . "', '" . $current_time . "', '" . $current_time . "', '" . $wo_last_page_url . "')");
-      xtc_db_query("insert into " . TABLE_WHOS_ONLINE . " (customer_id, full_name, session_id, ip_address, time_entry, time_last_click, last_page_url, http_referer) values ('" . $wo_customer_id . "', '" . $wo_full_name . "', '" . $wo_session_id . "', '" . $wo_ip_address . "', '" . $current_time . "', '" . $current_time . "', '" . $wo_last_page_url . "', '" . $wo_referer . "')");
+      xtc_db_query("
+      insert into " . TABLE_WHOS_ONLINE . "
+        (customer_id,
+        full_name,
+        session_id,
+        ip_address,
+        time_entry,
+        time_last_click,
+        last_page_url,
+        http_referer)
+      values (
+      '" . $wo_customer_id . "',
+      '" . $wo_full_name . "',
+      '" . $wo_session_id . "',
+      '" . $wo_ip_address . "',
+      '" . $current_time . "',
+      '" . $current_time . "',
+      '" . $wo_last_page_url . "',
+      '" . $wo_referer . "')"
+      );
     //BOF - Dokuman - 2009-10-28 - Who is online: added http_referer
     }
   }
