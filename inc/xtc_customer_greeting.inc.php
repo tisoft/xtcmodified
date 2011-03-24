cfcf2d0cf89c0c0a33bcb6f0a197dff235a14426
dokuman <dokuman@25741c12-d480-4641-b65d-acd752be3b7f>
2011-03-24 10:37:37 +0000

    remove customer's first name on greeting (can be set in template with {$smarty.session.customer_first_name} )
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1852 25741c12-d480-4641-b65d-acd752be3b7f

--- inc/xtc_customer_greeting.inc.php
+++ inc/xtc_customer_greeting.inc.php
@@ -1,16 +1,17 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: xtc_customer_greeting.inc.php 1009 2005-07-11 16:19:29Z mz $
+   $Id$
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
 
-   Copyright (c) 2003 XT-Commerce 
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(general.php,v 1.225 2003/05/29); www.oscommerce.com 
-   (c) 2003	 nextcommerce (xtc_customer_greeting.inc.php,v 1.3 2003/08/13); www.nextcommerce.org 
+   (c) 2003 nextcommerce (xtc_customer_greeting.inc.php,v 1.3 2003/08/13); www.nextcommerce.org 
+   (c) 2006 XT-Commerce (xtc_customer_greeting.inc.php 1009 2005-07-11)
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
@@ -20,16 +21,25 @@
 
     if (isset($_SESSION['customer_last_name']) && isset($_SESSION['customer_id'])) {
       if (!isset($_SESSION['customer_gender'])) {
-      $check_customer_query = "select customers_gender FROM  " . TABLE_CUSTOMERS . " where customers_id = '" . $_SESSION['customer_id'] . "'";
-      $check_customer_query = xtDBquery($check_customer_query);
-      $check_customer_data  = xtc_db_fetch_array($check_customer_query,true);
-      $_SESSION['customer_gender'] = $check_customer_data['customers_gender'];
+        $check_customer_query = "select customers_gender FROM  " . TABLE_CUSTOMERS . " where customers_id = '" . (int)$_SESSION['customer_id'] . "'";
+        $check_customer_query = xtDBquery($check_customer_query);
+        $check_customer_data  = xtc_db_fetch_array($check_customer_query,true);
+        $_SESSION['customer_gender'] = $check_customer_data['customers_gender'];
       }
-      if($_SESSION['customer_gender']=='f'){
-      $greeting_string = sprintf(TEXT_GREETING_PERSONAL, FEMALE . '&nbsp;'. $_SESSION['customer_first_name'] . '&nbsp;'. $_SESSION['customer_last_name'], xtc_href_link(FILENAME_PRODUCTS_NEW));
-      }else{
-      $greeting_string = sprintf(TEXT_GREETING_PERSONAL, MALE . '&nbsp;'. $_SESSION['customer_first_name'] . '&nbsp;' . $_SESSION['customer_last_name'], xtc_href_link(FILENAME_PRODUCTS_NEW));
+      //BOF - DokuMan - 2011-03-24 - remove customer's first name on greeting (can be set in template with {$smarty.session.customer_first_name} )
+      /*
+      if ($_SESSION['customer_gender'] == 'f') {
+        $greeting_string = sprintf(TEXT_GREETING_PERSONAL, FEMALE . '&nbsp;'. $_SESSION['customer_first_name'] . '&nbsp;'. $_SESSION['customer_last_name'], xtc_href_link(FILENAME_PRODUCTS_NEW));
+      } else {
+        $greeting_string = sprintf(TEXT_GREETING_PERSONAL, MALE . '&nbsp;'. $_SESSION['customer_first_name'] . '&nbsp;' . $_SESSION['customer_last_name'], xtc_href_link(FILENAME_PRODUCTS_NEW));
       }
+      */
+      if ($_SESSION['customer_gender'] == 'f') {
+        $greeting_string = sprintf(TEXT_GREETING_PERSONAL, FEMALE . '&nbsp;'. $_SESSION['customer_last_name'], xtc_href_link(FILENAME_PRODUCTS_NEW));
+      } else {
+        $greeting_string = sprintf(TEXT_GREETING_PERSONAL, MALE . '&nbsp;' . $_SESSION['customer_last_name'], xtc_href_link(FILENAME_PRODUCTS_NEW));
+      }
+      //EOF - DokuMan - 2011-03-24 - remove customer's first name on greeting (can be set in template with {$smarty.session.customer_first_name} )
 
     } else {
       $greeting_string = sprintf(TEXT_GREETING_GUEST, xtc_href_link(FILENAME_LOGIN, '', 'SSL'), xtc_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL'));
@@ -37,4 +47,4 @@
 
     return $greeting_string;
   }
- ?>
+?>
