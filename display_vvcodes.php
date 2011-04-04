228453ad6f54df1d6db1d2452d2c9bc82c792989
gtb-modified <gtb-modified@25741c12-d480-4641-b65d-acd752be3b7f>
2011-04-04 15:08:33 +0000

    minimize problems with Captcha, caused of whitespaces
    git-svn-id: https://xtcmodified.svn.sourceforge.net/svnroot/xtcmodified/trunk@1872 25741c12-d480-4641-b65d-acd752be3b7f

--- display_vvcodes.php
+++ display_vvcodes.php
@@ -1,15 +1,27 @@
 <?php
-
 /* -----------------------------------------------------------------------------------------
-  $Id: display_vvcodes.php 831 2005-03-13 10:16:09Z mz $   
+   $Id$
 
-   Copyright (c) 2004 XT-Commerce
+   xtcModified - community made shopping
+   http://www.xtc-modified.org
+
+   Copyright (c) 2010 xtcModified
    -----------------------------------------------------------------------------------------
+   based on:
+   (c) 2006 XT-Commerce
   
    Released under the GNU General Public License  
    ---------------------------------------------------------------------------------------*/
 
-require ('includes/application_top.php');
+//BOF - GTB - 2011-04-04 - only include configure to minimize Problems with Captcha
+//require ('includes/application_top.php');
+if (file_exists('includes/local/configure.php')) {
+	include ('includes/local/configure.php');
+} else {
+	include ('includes/configure.php');
+}
+//EOF - GTB - 2011-04-04 - only include configure to minimize Problems with Captcha
+
 require_once (DIR_FS_INC.'xtc_render_vvcode.inc.php');
 require_once (DIR_FS_INC.'xtc_random_charcode.inc.php');
 
